aba342e68675f822c3fe0ec3d63f20a46c44b0bf
Ryan Moran <ryan.moran@gmail.com>
2012-01-10 14:24:33 -0800

    adding namespace for bill address

--- lib/netsuite/records/bill_address.rb
+++ lib/netsuite/records/bill_address.rb
@@ -2,6 +2,8 @@ module NetSuite
   module Records
     class BillAddress
       include Support::Fields
+      include Support::Records
+      include Namespaces::PlatformCommon
 
       fields :bill_attention, :bill_addressee, :bill_phone, :bill_addr1, :bill_addr2,
         :bill_addr3, :bill_city, :bill_state, :bill_zip, :bill_country
--- spec/netsuite/records/bill_address_spec.rb
+++ spec/netsuite/records/bill_address_spec.rb
@@ -12,4 +12,41 @@ describe NetSuite::Records::BillAddress do
     end
   end
 
+  describe '#to_record' do
+    before do
+      bill_address.bill_attention = 'Mr. Smith'
+      bill_address.bill_addressee = 'Mr. Robert Smith'
+      bill_address.bill_phone     = '1234567890'
+      bill_address.bill_addr1     = '123 Happy Lane'
+      bill_address.bill_addr2     = '#4'
+      bill_address.bill_addr3     = 'Box 6'
+      bill_address.bill_city      = 'Los Angeles'
+      bill_address.bill_state     = 'CA'
+      bill_address.bill_zip       = '90007'
+      bill_address.bill_country   = '_unitedStates'
+    end
+
+    it 'can represent itself as a SOAP record' do
+      record = {
+        'platformCommon:billAttention' => 'Mr. Smith',
+        'platformCommon:billAddressee' => 'Mr. Robert Smith',
+        'platformCommon:billPhone'     => '1234567890',
+        'platformCommon:billAddr1'     => '123 Happy Lane',
+        'platformCommon:billAddr2'     => '#4',
+        'platformCommon:billAddr3'     => 'Box 6',
+        'platformCommon:billCity'      => 'Los Angeles',
+        'platformCommon:billState'     => 'CA',
+        'platformCommon:billZip'       => '90007',
+        'platformCommon:billCountry'   => '_unitedStates'
+      }
+      bill_address.to_record.should eql(record)
+    end
+  end
+
+  describe 'record_namespace' do
+    it 'belongs to the platformCommon namespace' do
+      bill_address.record_namespace.should eql('platformCommon')
+    end
+  end
+
 end
