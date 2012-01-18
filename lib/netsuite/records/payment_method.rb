57d35bc9e4acb284389f055701dd34e6b8ab72cd
Ryan Moran <ryan.moran@gmail.com>
2012-01-18 15:48:16 -0800

    adding internal and external ids for payment methods

--- lib/netsuite/records/payment_method.rb
+++ lib/netsuite/records/payment_method.rb
@@ -9,7 +9,12 @@ module NetSuite
 
       record_ref :account
 
+      attr_reader :internal_id
+      attr_accessor :external_id
+
       def initialize(attributes = {})
+        @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
+        @external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
         initialize_from_attributes_hash(attributes)
       end
 
