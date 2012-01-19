a83cd04d93dac3f3898ee914a403dbaaaf79044b
Ryan Moran <ryan.moran@gmail.com>
2012-01-18 17:54:44 -0800

    get for locations

--- lib/netsuite/records/location.rb
+++ lib/netsuite/records/location.rb
@@ -9,8 +9,22 @@ module NetSuite
 
       record_refs :logo, :parent
 
+      attr_reader :internal_id
+      attr_accessor :external_id
+
       def initialize(attributes = {})
+        @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
+        @external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
         initialize_from_attributes_hash(attributes)
+      end
+
+      def self.get(options = {})
+        response = Actions::Get.call(self, options)
+        if response.success?
+          new(response.body)
+        else
+          raise RecordNotFound, "#{self} with OPTIONS=#{options.inspect} could not be found"
+        end
       end
 
     end
--- spec/netsuite/records/location_spec.rb
+++ spec/netsuite/records/location_spec.rb
@@ -23,4 +23,29 @@ describe NetSuite::Records::Location do
     end
   end
 
+  describe '.get' do
+    context 'when the response is successful' do
+      let(:response) { NetSuite::Response.new(:success => true, :body => { :city => 'Los Angeles' }) }
+
+      it 'returns a Location instance populated with the data from the response object' do
+        NetSuite::Actions::Get.should_receive(:call).with(NetSuite::Records::Location, :external_id => 1).and_return(response)
+        location = NetSuite::Records::Location.get(:external_id => 1)
+        location.should be_kind_of(NetSuite::Records::Location)
+        location.city.should eql('Los Angeles')
+      end
+    end
+
+    context 'when the response is unsuccessful' do
+      let(:response) { NetSuite::Response.new(:success => false, :body => {}) }
+
+      it 'raises a RecordNotFound exception' do
+        NetSuite::Actions::Get.should_receive(:call).with(NetSuite::Records::Location, :external_id => 1).and_return(response)
+        lambda {
+          NetSuite::Records::Location.get(:external_id => 1)
+        }.should raise_error(NetSuite::RecordNotFound,
+          /NetSuite::Records::Location with OPTIONS=(.*) could not be found/)
+      end
+    end
+  end
+
 end
