15ebfbbe7a567d5447eab9f2dbeaceea13ba379a
Ryan Moran <ryan.moran@gmail.com>
2012-01-18 17:51:16 -0800

    location with record refs

--- lib/netsuite/records/location.rb
+++ lib/netsuite/records/location.rb
@@ -2,9 +2,12 @@ module NetSuite
   module Records
     class Location
       include Support::Fields
+      include Support::RecordRefs
 
       fields :addr1, :addr2, :addr3, :addr_phone, :addr_text, :addressee, :attention, :city, :country, :include_children,
         :is_inactive, :make_inventory_available, :make_inventory_available_store, :name, :override, :state, :tran_prefix, :zip
+
+      record_refs :logo, :parent
 
       def initialize(attributes = {})
         initialize_from_attributes_hash(attributes)
