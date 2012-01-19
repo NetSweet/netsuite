859c2f5d35d3d081c0a828166487aca176e43fe9
Ryan Moran <ryan.moran@gmail.com>
2012-01-19 15:44:08 -0800

    internal and external ids for inventory item

--- lib/netsuite/records/inventory_item.rb
+++ lib/netsuite/records/inventory_item.rb
@@ -37,7 +37,12 @@ module NetSuite
         :stock_unit, :store_display_image, :store_display_thumbnail, :store_item_template, :supply_lot_sizing_method,
         :supply_replenishment_method, :supply_type, :tax_schedule, :units_type, :vendor
 
+      attr_reader :internal_id
+      attr_accessor :external_id
+
       def initialize(attributes = {})
+        @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
+        @external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
         initialize_from_attributes_hash(attributes)
       end
 
