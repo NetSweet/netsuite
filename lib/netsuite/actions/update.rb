354572e5763998ae0db88d88d1c76669d4362c1a
Ryan Moran <ryan.moran@gmail.com>
2012-01-23 16:44:51 -0800

    adding namespaces for update

--- lib/netsuite/actions/update.rb
+++ lib/netsuite/actions/update.rb
@@ -16,6 +16,8 @@ module NetSuite
           soap.namespaces['xmlns:tranSales']      = 'urn:sales_2011_2.transactions.webservices.netsuite.com'
           soap.namespaces['xmlns:platformCommon'] = 'urn:common_2011_2.platform.webservices.netsuite.com'
           soap.namespaces['xmlns:listAcct']       = 'urn:accounting_2011_2.lists.webservices.netsuite.com'
+          soap.namespaces['xmlns:tranCust']       = 'urn:customers_2011_2.transactions.webservices.netsuite.com'
+          soap.namespaces['xmlns:setupCustom']    = 'urn:customization_2011_2.setup.webservices.netsuite.com'
           soap.header = auth_header
           soap.body   = request_body
         end
