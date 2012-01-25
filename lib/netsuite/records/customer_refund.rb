7837e5e40dc11a1adb7aa81fcd6f8c6ae744a409
Monica McArthur <monica@revolutionprep.com>
2012-01-24 19:46:17 -0800

    Balance needs to be read-only in customer refund

--- lib/netsuite/records/customer_refund.rb
+++ lib/netsuite/records/customer_refund.rb
@@ -9,7 +9,7 @@ module NetSuite
 
       actions :get, :initialize, :add, :delete
 
-      fields :address, :balance, :cc_approved, :cc_expire_date, :cc_name, :cc_number, :cc_street, :cc_zip_code, :charge_it,
+      fields :address, :cc_approved, :cc_expire_date, :cc_name, :cc_number, :cc_street, :cc_zip_code, :charge_it,
         :created_date, :currency_name, :debit_card_issue_no, :exchange_rate, :last_modified_date, :memo, :pn_ref_num, :status,
         :to_be_printed, :tran_date, :tran_id, :valid_from
 
@@ -17,7 +17,7 @@ module NetSuite
       field :apply_list,        CustomerRefundApplyList
       field :deposit_list,      CustomerRefundDepositList
 
-      read_only_fields :total
+      read_only_fields :balance, :total
 
       record_refs :account, :ar_acct, :credit_card, :credit_card_processor, :custom_form, :customer, :department, :klass,
         :location, :payment_method, :posting_period, :subsidiary, :void_journal
