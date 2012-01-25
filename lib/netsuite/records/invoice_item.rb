e82da4722c7e4d2a38927d6157a0d207890f44ed
Ryan Moran <ryan.moran@gmail.com>
2012-01-25 13:35:44 -0800

    adding custom field list to invoice item

--- lib/netsuite/records/invoice_item.rb
+++ lib/netsuite/records/invoice_item.rb
@@ -6,13 +6,15 @@ module NetSuite
       include Support::Records
       include Namespaces::TranSales
 
-      fields :amount, :amount_ordered, :bin_numbers, :cost_estimate, :cost_estimate_type, :current_percent, :custom_field_list,
+      fields :amount, :amount_ordered, :bin_numbers, :cost_estimate, :cost_estimate_type, :current_percent,
         :defer_rev_rec, :description, :gift_cert_from, :gift_cert_message, :gift_cert_number, :gift_cert_recipient_email,
         :gift_cert_recipient_name, :gross_amt, :inventory_detail, :is_taxable, :item_is_fulfilled, :license_code, :line,
-        :options, :order_line, :percent_complete, :quantity, :quantity_available, :quantity_fulfilled, :quantity_on_hand,
-        :quantity_ordered, :quantity_remaining, :rate, :rev_rec_end_date, :rev_rec_start_date, :serial_numbers, :ship_group,
-        :tax1_amt, :tax_rate1, :tax_rate2, :vsoe_allocation, :vsoe_amount, :vsoe_deferral, :vsoe_delivered,
-        :vsoe_permit_discount, :vsoe_price
+        :klass, :options, :order_line, :percent_complete, :quantity, :quantity_available, :quantity_fulfilled,
+        :quantity_on_hand, :quantity_ordered, :quantity_remaining, :rate, :rev_rec_end_date, :rev_rec_start_date,
+        :serial_numbers, :ship_group, :tax1_amt, :tax_rate1, :tax_rate2, :vsoe_allocation, :vsoe_amount, :vsoe_deferral,
+        :vsoe_delivered, :vsoe_permit_discount, :vsoe_price
+
+      field :custom_field_list, CustomFieldList
 
       record_refs :department, :item, :job, :location, :price, :rev_rec_schedule, :ship_address, :ship_method, :tax_code, :units
 
@@ -27,6 +29,14 @@ module NetSuite
 
       def initialize_from_record(record)
         self.attributes = record.send(:attributes)
+      end
+
+      def to_record
+        rec = super
+        if rec["#{record_namespace}:customFieldList"]
+          rec["#{record_namespace}:customFieldList!"] = rec.delete("#{record_namespace}:customFieldList")
+        end
+        rec
       end
 
     end
--- spec/netsuite/records/invoice_item_spec.rb
+++ spec/netsuite/records/invoice_item_spec.rb
@@ -1,18 +1,19 @@
 require 'spec_helper'
 
 describe NetSuite::Records::InvoiceItem do
-  let(:list) { NetSuite::Records::InvoiceItem.new }
+  let(:item) { NetSuite::Records::InvoiceItem.new }
 
   it 'has the right fields' do
     [
-      :amount, :amount_ordered, :bin_numbers, :cost_estimate, :cost_estimate_type, :current_percent, :custom_field_list,
-      :defer_rev_rec, :description, :gift_cert_from, :gift_cert_message, :gift_cert_number, :gift_cert_recipient_email,
-      :gift_cert_recipient_name, :gross_amt, :inventory_detail, :is_taxable, :item_is_fulfilled, :license_code, :line, :options,
-      :order_line, :percent_complete, :quantity, :quantity_available, :quantity_fulfilled, :quantity_on_hand, :quantity_ordered,
-      :quantity_remaining, :rate, :rev_rec_end_date, :rev_rec_start_date, :serial_numbers, :ship_group, :tax1_amt, :tax_rate1,
-      :tax_rate2, :vsoe_allocation, :vsoe_amount, :vsoe_deferral, :vsoe_delivered, :vsoe_permit_discount, :vsoe_price
+      :amount, :amount_ordered, :bin_numbers, :cost_estimate, :cost_estimate_type, :current_percent, :defer_rev_rec,
+      :description, :gift_cert_from, :gift_cert_message, :gift_cert_number, :gift_cert_recipient_email,
+      :gift_cert_recipient_name, :gross_amt, :inventory_detail, :is_taxable, :item_is_fulfilled, :license_code, :line, :klass,
+      :options, :order_line, :percent_complete, :quantity, :quantity_available, :quantity_fulfilled, :quantity_on_hand,
+      :quantity_ordered, :quantity_remaining, :rate, :rev_rec_end_date, :rev_rec_start_date, :serial_numbers, :ship_group,
+      :tax1_amt, :tax_rate1, :tax_rate2, :vsoe_allocation, :vsoe_amount, :vsoe_deferral, :vsoe_delivered, :vsoe_permit_discount,
+      :vsoe_price
     ].each do |field|
-      list.should have_field(field)
+      item.should have_field(field)
     end
   end
 
@@ -20,24 +21,41 @@ describe NetSuite::Records::InvoiceItem do
     [
       :department, :item, :job, :location, :price, :rev_rec_schedule, :ship_address, :ship_method, :tax_code, :units
     ].each do |record_ref|
-      list.should have_record_ref(record_ref)
+      item.should have_record_ref(record_ref)
     end
   end
 
   it 'can initialize from a record' do
     record = NetSuite::Records::InvoiceItem.new(:amount => 123, :cost_estimate => 234)
-    list   = NetSuite::Records::InvoiceItem.new(record)
-    list.should be_kind_of(NetSuite::Records::InvoiceItem)
-    list.amount.should eql(123)
-    list.cost_estimate.should eql(234)
+    item   = NetSuite::Records::InvoiceItem.new(record)
+    item.should be_kind_of(NetSuite::Records::InvoiceItem)
+    item.amount.should eql(123)
+    item.cost_estimate.should eql(234)
   end
 
-  # :class RecordRef
+  describe '#custom_field_list' do
+    it 'can be set from attributes' do
+      attributes = {
+        :custom_field => {
+          :value => 10
+        }
+      }
+      item.custom_field_list = attributes
+      item.custom_field_list.should be_kind_of(NetSuite::Records::CustomFieldList)
+      item.custom_field_list.custom_fields.length.should eql(1)
+    end
+
+    it 'can be set from a CustomFieldList object' do
+      custom_field_list = NetSuite::Records::CustomFieldList.new
+      item.custom_field_list = custom_field_list
+      item.custom_field_list.should eql(custom_field_list)
+    end
+  end
 
   describe '#to_record' do
     before do
-      list.amount      = '7'
-      list.description = 'Some thingy'
+      item.amount      = '7'
+      item.description = 'Some thingy'
     end
 
     it 'can represent itself as a SOAP record' do
@@ -45,13 +63,13 @@ describe NetSuite::Records::InvoiceItem do
         'tranSales:amount'      => '7',
         'tranSales:description' => 'Some thingy'
       }
-      list.to_record.should eql(record)
+      item.to_record.should eql(record)
     end
   end
 
   describe '#record_type' do
     it 'returns a string of the SOAP record type' do
-      list.record_type.should eql('tranSales:InvoiceItem')
+      item.record_type.should eql('tranSales:InvoiceItem')
     end
   end
 
