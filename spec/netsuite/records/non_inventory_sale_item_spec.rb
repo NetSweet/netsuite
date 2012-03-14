59f3d17762f00be82c2b7857167b2e94123e6a03
Tom Head <tom.head@revolutionprep.com>
2012-03-14 13:34:00 -0700

    Typo

--- spec/netsuite/records/non_inventory_sale_item_spec.rb
+++ spec/netsuite/records/non_inventory_sale_item_spec.rb
@@ -26,7 +26,7 @@ describe NetSuite::Records::NonInventorySaleItem do
 
   it 'has the right record_refs' do
     [
-      :billing_schedule, :cost_category, :custom_form, :deferred_revenue_account, :department, :income_amount, :issue_product,
+      :billing_schedule, :cost_category, :custom_form, :deferred_revenue_account, :department, :income_account, :issue_product,
       :item_options_list, :klass, :location, :parent, :pricing_group, :purchase_tax_code, :quantity_pricing_schedule,
       :rev_rec_schedule, :sale_unit, :sales_tax_code, :ship_package, :store_display_image, :store_display_thumbnail,
       :store_item_template, :subsidiary_list, :tax_schedule, :units_type
