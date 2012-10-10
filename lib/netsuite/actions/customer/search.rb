b9f6b97681757932f22d16847c15e81b001275fc
David Pham <davidpham@new-host-2.home>
2012-10-10 12:19:00 -0400

    Added specific request body to Customer Search class

--- lib/netsuite/actions/customer/search.rb
+++ lib/netsuite/actions/customer/search.rb
@@ -1,3 +1,5 @@
+# TODO: Tests
+# TODO: DBC
 module NetSuite
 	module Actions
 		module Customer
@@ -14,11 +16,38 @@ module NetSuite
 
 				def call
 					response = NetSuite::Configuration.connection.request :search do
+						soap.namespaces['xmlns:platformMsgs'] = "urn:messages_#{NetSuite::Configuration.api_version}.platform.webservices.netsuite.com"
 						soap.namespaces['xmlns:platformCore'] = "urn:core_#{NetSuite::Configuration.api_version}.platform.webservices.netsuite.com"
+						soap.namespaces['xmlns:listRel'] = "urn:relationships_#{NetSuite::Configuration.api_version}.lists.webservices.netsuite.com"
 						soap.header = NetSuite::Configuration.auth_header
-						soap.body = fields
+						soap.body = request_body(@fields)
 					end
 				end
+
+				private
+
+				def soap_type(object)
+        	object.klass.to_s.split('::').last.lower_camelcase
+      	end
+
+	      def request_body(fields)
+	        buffer = ''
+        	
+        	record_type = soap_type(self)
+        	
+        	xml = Builder::XmlMarkup.new(target: buffer)
+        	
+        	xml.platformMsgs(:searchRecord, 'xsi:type' => 'listRel:CustomerSearch') do |customer_search|
+        		fields.each do |field_name, field_value|
+        			# TODO: allow for different search operators
+        			customer_search.listRel(field_name.to_sym, operator: 'contains') do |operator|
+        				operator.platformCore :searchValue, field_value
+        			end
+        		end
+        	end
+
+        	buffer
+	      end
 			end
 		end
 	end
