13525f5e4b73a185f75bfac56d5b8bfa3497d61c
David Pham <davidpham@new-host-2.home>
2012-10-11 16:50:29 -0400

    Revert "Added ability to search with other operators and types"
    This reverts commit 5f304ad7d9c9759979010414ec9949759c063952.

--- lib/netsuite/actions/search.rb
+++ lib/netsuite/actions/search.rb
@@ -38,12 +38,14 @@ module NetSuite
         # TODO: Consistent use of namespace qualifying
         xml.searchRecord('xsi:type' => @klass.custom_soap_search_record_type) do |search_record|
           search_record.basic('xsi:type' => "platformCommon:#{@klass.respond_to?(:custom_soap_basic_search_record_type) ? @klass.custom_soap_basic_search_record_type : soap_record_type}SearchBasic") do |basic|
-            @options.each do |field_name, field_options|
+            @options.each do |field_name, field_value|
+            	# TODO: Add ability to use other operators
+            	# TODO: Add ability to use other field types
               basic.method_missing(field_name, {
-                operator: field_options[:operator],
-                'xsi:type' => field_options[:type] || 'platformCore:SearchStringField'
+                operator: 'contains',
+                'xsi:type' => 'platformCore:SearchStringField'
               }) do |_field_name|
-                _field_name.platformCore :searchValue, field_options[:value]
+                _field_name.platformCore :searchValue, field_value
               end
             end
           end
