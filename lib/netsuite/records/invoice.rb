ea92db28614da53f241f04f02cc395044c05984c
David Pham <davidpham@David-Phams-iMac.local>
2013-02-01 15:18:19 -0500

    Changed invoice type to cash sale

--- lib/netsuite/records/invoice.rb
+++ lib/netsuite/records/invoice.rb
@@ -58,7 +58,7 @@ module NetSuite
             type: {
               type: 'platformCore:SearchEnumMultiSelectField',
               operator: 'anyOf',
-              value: '_invoice'
+              value: '_cashSale'
             }
           }
         }
