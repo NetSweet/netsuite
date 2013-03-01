ed1b5488bc10badccbb4a80ff9272800c07df8b7
Michael Bianco <iloveitaly@gmail.com>
2013-03-01 15:36:06 -0500

    Adding search support to contact + customer
    Removing sales order support as well

--- lib/netsuite/records/contact.rb
+++ lib/netsuite/records/contact.rb
@@ -7,7 +7,7 @@ module NetSuite
       include Support::Actions
       include Namespaces::ListRel
 
-      actions :get, :add, :delete
+      actions :get, :add, :delete, :search
 
       fields :salutation, :first_name, :middle_name, :last_name, :title, :phone, :fax, :email, :default_address,
              :entity_id, :phonetic_name, :alt_email, :office_phone, :home_phone, :mobile_phone, :supervisor_phone,
