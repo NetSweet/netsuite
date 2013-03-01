1ce492448776d53b67a0738e77795625a2021ab8
Michael Bianco <iloveitaly@gmail.com>
2013-03-01 13:18:55 -0500

    Dynamic search parameters working

--- lib/netsuite/actions/search.rb
+++ lib/netsuite/actions/search.rb
@@ -24,9 +24,12 @@ module NetSuite
           soap.namespaces['xmlns:listRel'] = "urn:relationships_#{NetSuite::Configuration.api_version}.lists.webservices.netsuite.com"
           soap.namespaces['xmlns:tranSales'] = "urn:sales_#{NetSuite::Configuration.api_version}.transactions.webservices.netsuite.com"
 
+          # https://system.netsuite.com/help/helpcenter/en_US/Output/Help/SuiteFlex/WebServices/STP_SettingSearchPreferences.html#N2028598271-3
           soap.header = auth_header.merge({
             'platformMsgs:searchPreferences' => {
-              'pageSize' => 5
+              'pageSize' => 5,
+              'bodyFieldsOnly' => true,
+              'returnSearchColumns' => true   # default is true; only valid for advanced searches
             }
           })
 
@@ -43,43 +46,43 @@ module NetSuite
 
         # TODO: When searching invoices allow for multiple basic search criteria to be set
         # TODO: Make setting of criteria and columns easier
-        xml.searchRecord('xsi:type' => @klass.custom_soap_advanced_search_record_type) do |search_record|
-          search_record.criteria do |criteria|
-            if @klass.respond_to?(:default_search_options)
-              @options[:criteria].merge!(@klass.default_search_options)
-            end
+        # xml.searchRecord('xsi:type' => @klass.custom_soap_advanced_search_record_type) do |search_record|
+        #   search_record.criteria do |criteria|
+        #     if @klass.respond_to?(:default_search_options)
+        #       @options[:criteria].merge!(@klass.default_search_options)
+        #     end
 
-            @options[:criteria].each do |criteria_type, _criteria|
-              criteria.method_missing(criteria_type) do |_criteria_type|
-                _criteria.each do |criteria_name, criteria_options|
-                  criteria_hash = {
-                    'xsi:type' => criteria_options[:type]
-                  }
+        #     @options[:criteria].each do |criteria_type, _criteria|
+        #       criteria.method_missing(criteria_type) do |_criteria_type|
+        #         _criteria.each do |criteria_name, criteria_options|
+        #           criteria_hash = {
+        #             'xsi:type' => criteria_options[:type]
+        #           }
 
-                  if !!criteria_options[:operator] && !criteria_options[:operator].empty?
-                    criteria_hash.merge!({ operator: criteria_options[:operator] })
-                  end
+        #           if !!criteria_options[:operator] && !criteria_options[:operator].empty?
+        #             criteria_hash.merge!({ operator: criteria_options[:operator] })
+        #           end
 
-                  _criteria_type.method_missing(criteria_name, criteria_hash) do |_criteria_name|
-                    _criteria_name.platformCore(:searchValue, criteria_options[:value])
-                  end
-                end
-              end
-            end
-          end
+        #           _criteria_type.method_missing(criteria_name, criteria_hash) do |_criteria_name|
+        #             _criteria_name.platformCore(:searchValue, criteria_options[:value])
+        #           end
+        #         end
+        #       end
+        #     end
+        #   end
 
-          search_record.columns do |columns|
-            @options[:columns].each do |result_type, result_columns|
-              columns.method_missing(result_type) do |_result_type|
-                result_columns.each do |result_column|
-                  _result_type.method_missing(result_column)
-                end
-              end
-            end if !!@options[:columns] && !@options[:columns].empty?
-          end
-        end
+        #   search_record.columns do |columns|
+        #     @options[:columns].each do |result_type, result_columns|
+        #       columns.method_missing(result_type) do |_result_type|
+        #         result_columns.each do |result_column|
+        #           _result_type.method_missing(result_column)
+        #         end
+        #       end
+        #     end if !!@options[:columns] && !@options[:columns].empty?
+        #   end
+        # end
 
-        buffer
+        # buffer
 
         # <soap:Body>
         # <platformMsgs:search>
@@ -96,58 +99,62 @@ module NetSuite
         # this is the hash that the above code needs to spit out
         # I don't want to have to create seperate objects for *everything*
 
-        # binding.pry
+        # criteria: {
+        #   basic: {
+        #     [
+        #       {
+        #         field: 'email'
+        #         operator: 'is',
+        #         value: 'Michael'
+        #       }
+        #     ]
+        #   },
+        # },
+
         example = @klass.new
         namespace = example.record_namespace
         class_name = @klass.to_s.split("::").last
 
-        {
-          'searchRecord' => {
-            'listRel:customerJoin' => {
-              'platformCommon:email' => {
-                'platformCore:searchValue' => 'gmail.com'
-              },
-              :attributes! => {
-                'platformCommon:email' => {
-                  'operator' => 'contains',
-                  'xsi:type' => 'platformCore:SearchStringField'
-                }
-              }
-            },
-            :attributes! => {
-              'customerJoin' => {
-                'xsi:type' => "listRel:CustomerSearchBasic"
-              }
+        elements = {}
+        attributes = {}
+
+        @options[:criteria].each_pair do |condition_category, conditions|
+          elements["#{namespace}:#{condition_category}"] = conditions.inject({}) do |h, condition|
+            binding.pry
+            h["platformCommon:#{condition[:field]}"] = {
+              "platformCore:searchValue" => condition[:value]
             }
-          },
-          :attributes! => {
-            'searchRecord' => {
-              'xsi:type' => 'listRel:ContactSearch'
-            },
-          }
-        }
+
+            h[:attributes!] ||= {}
+
+            h[:attributes!]["platformCommon:#{condition[:field]}"] = {
+              'operator' => condition[:operator],
+              'xsi:type' => 'platformCore:SearchStringField'
+            }
+
+            h
+          end
+
+          if condition_category.to_s.include? 'Join'
+            join_class = condition_category.to_s.split('Join').first.capitalize
+
+            (elements[:attributes!] ||= {}).merge({
+              "#{condition_category}" => {
+                'xsi:type' => "#{namespace}:#{join_class}SearchBasic"
+              }
+            })
+
+          end
+
+        end
+
+        binding.pry
 
         # customer search
+        # https://webservices.netsuite.com/xsd/lists/v2012_2_0/relationships.xsd
 
         {
-          'searchRecord' => {
-            "#{namespace}:basic" => {
-              'platformCommon:email' => {
-                'platformCore:searchValue' => 'gmail.com'
-              },
-              :attributes! => {
-                'platformCommon:email' => {
-                  'operator' => 'contains',
-                  'xsi:type' => 'platformCore:SearchStringField'
-                }
-              }
-            },
-            :attributes! => {
-              'customerJoin' => {
-                'xsi:type' => "#{namespace}:#{class_name}SearchBasic"
-              }
-            }
-          },
+          'searchRecord' => elements,
           :attributes! => {
             'searchRecord' => {
               # ex: listRel:CustomerSearch
