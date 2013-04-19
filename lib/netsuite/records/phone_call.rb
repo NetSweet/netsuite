88a29ec0ad22935865b493704a6a336837ea20a4
John Terry <jpterry@gmail.com>
2013-04-18 18:22:04 -0700

    Add PhoneCall record

---
+++ lib/netsuite/records/phone_call.rb
@@ -0,0 +1,29 @@
+module NetSuite
+  module Records
+    class PhoneCall
+      include Support::Fields
+      include Support::RecordRefs
+      include Support::Records
+      include Support::Actions
+      include Namespaces::ActSched
+
+      actions :get, :add, :delete, :update
+
+      fields :title, :message, :phone, :status, :priority, :start_date, :end_date,
+        :start_time, :end_time, :completed_date, :timed_event, :access_level, :timed_event
+
+      field :contact_list, ContactList
+
+      record_refs :assigned, :owner, :company, :contact
+
+      attr_reader   :internal_id
+      attr_accessor :external_id
+
+      def initialize(attributes = {})
+        @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
+        @external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
+        initialize_from_attributes_hash(attributes)
+      end
+    end
+  end
+end
