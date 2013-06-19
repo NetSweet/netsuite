NetSweet/netsuite

commit 870758eb827dd3201b7e8639fd68b60852091f69

Setting custom field list values is now working

lib/netsuite/records/custom_field_list.rb
@@ -30,14 +30,18 @@ module NetSuite
       end
 
       def to_record
-        custom_fields.inject([]) do |list, custom_field|
-          list << {
-            "#{record_namespace}:customField" => custom_field.to_record,
-            :attributes! => {
-              "#{record_namespace}:customField" => custom_field.attributes!
+        # TODO this is the best way I could find to handle this, there *has* to be a better way
+        # http://stackoverflow.com/questions/7001957/savon-array-of-xml-tags
+
+        {
+          "#{record_namespace}:customField" => custom_fields.map(&:to_record),
+          :attributes! => {
+            "#{record_namespace}:customField" => {
+              'internalId' => custom_fields.map(&:internal_id),
+              'xsi:type' => custom_fields.map(&:type)
             }
           }
-        end
+        }
       end
 
       private

lib/netsuite/records/custom_field.rb
@@ -16,23 +16,6 @@ module NetSuite
         attributes[:value]
       end
 
-      def to_record
-        attributes.inject({}) do |hash, (k,v)|
-          kname = "#{record_namespace}:#{k.to_s.lower_camelcase}"
-          to_attributes!(hash, kname, v)
-          v = v.to_record if v.respond_to?(:to_record)
-          hash[kname] = v
-          hash
-        end
-      end
-
-      def attributes!
-        attr_hash = {}
-        attr_hash['internalId'] = internal_id if internal_id
-        attr_hash['xsi:type']   = type        if type
-        attr_hash
-      end
-
     end
   end
 end

commit 64a0719cbb7060edf4674b51ac8403f9ece76393

fix custom field to_record
It was broken in commit 870758e.

lib/netsuite/records/custom_field_list.rb
@@ -30,18 +30,15 @@ module NetSuite
       end
 
       def to_record
-        # TODO this is the best way I could find to handle this, there *has* to be a better way
-        # http://stackoverflow.com/questions/7001957/savon-array-of-xml-tags
-
-        {
-          "#{record_namespace}:customField" => custom_fields.map(&:to_record),
-          :attributes! => {
+        custom_fields.map do |custom_field|
+          {
             "#{record_namespace}:customField" => {
-              'internalId' => custom_fields.map(&:internal_id),
-              'xsi:type' => custom_fields.map(&:type)
+              :content! => custom_field.value.to_s,
+              '@internalId' => custom_field.internal_id,
+              '@xsi:type' => custom_field.type
             }
           }
-        }
+        end
       end
 
       private

spec/netsuite/records/custom_field_list_spec.rb
@@ -14,19 +14,16 @@ describe NetSuite::Records::CustomFieldList do
         :type        => 'BooleanCustomFieldRef',
         :value       => false
       )
+      list.custom_fields << NetSuite::Records::CustomField.new(
+        :internal_id => '4',
+        :type        => 'BooleanCustomFieldRef',
+        :value       => true
+      )
     end
 
     it 'can represent itself as a SOAP record' do
-      record = [
-        {
-          "platformCore:customField" => {"platformCore:value"=>false},
-          :attributes! => {
-            "platformCore:customField" => {"internalId"=>"3", "xsi:type"=>"BooleanCustomFieldRef"}
-          }
-        }
-      ]
-      
-      list.to_record.should eql(record)
+      list.to_record.length.should == 2
+      Gyoku.xml(list.to_record[0]).should == '<platformCore:customField internalId="3" xsi:type="BooleanCustomFieldRef">false</platformCore:customField>'
     end
   end
 