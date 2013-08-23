98553919ddf1bc7ecc0678e42d8c8f1a0642c289
Michael Bianco <iloveitaly@gmail.com>
2013-08-23 14:00:12 -0400

    Removing transaction record definition details; inheriting from SalesOrder
    Not too familiar with the differences between SalesOrder and Transaction
    but they seem to be similiar enough

--- lib/netsuite/records/transaction.rb
+++ lib/netsuite/records/transaction.rb
@@ -2,25 +2,7 @@
 
 module NetSuite
   module Records
-    class Transaction
-      include Support::Fields
-      include Support::RecordRefs
-      include Support::Records
-      include Support::Actions
-      include Namespaces::TranSales
-
-      actions :get, :initialize, :add, :delete, :search
-
-      record_refs :klass
-
-      attr_reader   :internal_id
-      attr_accessor :external_id
-
-      def initialize(attributes = {})
-        @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
-        @external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
-        initialize_from_attributes_hash(attributes)
-      end
+    class Transaction < SalesOrder
 
       def to_record
         rec = super
