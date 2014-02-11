3f6ca07ca89a5144f52d8f87eda986642e8614e8
Washington Luiz <huoxito@gmail.com>
2014-02-11 17:31:09 -0300

    Add is_closed to SalesOrderItem
    Setting that to true allow us to close a sales order successfully

--- lib/netsuite/records/sales_order_item.rb
+++ lib/netsuite/records/sales_order_item.rb
@@ -1,5 +1,8 @@
 module NetSuite
   module Records
+    # Beaware of possible gotcha when closing sales order. You need to set the
+    # SalesOrder#custom_form to "Basic Sales Order Form" otherwise you might get
+    # errors when trying to close it
     class SalesOrderItem
       include Support::Fields
       include Support::RecordRefs
@@ -10,7 +13,7 @@ module NetSuite
         :gift_cert_message, :gift_cert_number, :gift_cert_recipient_email, :gift_cert_recipient_name, :gross_amt, :is_taxable,
         :line, :order_line, :quantity, :rate, :rev_rec_end_date, :rev_rec_start_date, :rev_rec_term_in_months, :serial_numbers,
         :shipping_cost, :tax1_amt, :tax_rate1, :tax_rate2, :vsoe_allocation, :vsoe_amount, :vsoe_deferral, :vsoe_delivered,
-        :vsoe_permit_discount, :vsoe_price
+        :vsoe_permit_discount, :vsoe_price, :is_closed
 
       field :custom_field_list, CustomFieldList
 
@@ -36,7 +39,6 @@ module NetSuite
         end
         rec
       end
-
     end
   end
 end
