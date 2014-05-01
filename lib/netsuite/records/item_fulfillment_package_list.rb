4439ac30929c84a6b0cdf704740148a86e57dd6a
Washington Luiz <huoxito@gmail.com>
2014-05-01 17:41:37 -0300

    Always account for hash or array

--- lib/netsuite/records/item_fulfillment_package_list.rb
+++ lib/netsuite/records/item_fulfillment_package_list.rb
@@ -11,8 +11,19 @@ module NetSuite
         if attributes.keys != [:package] && attributes.first
 
           transformed_attrs = {}
-          attributes.first.last.each do |k, v|
-            transformed_attrs.merge!(k[0..-5].to_sym => v)
+          object = attributes.first.last
+
+          case object
+          when Hash
+            object.each do |k, v|
+              transformed_attrs.merge!(k[0..-5].to_sym => v)
+            end
+          when Array
+            object.each do |hash|
+              hash.each do |k, v|
+                transformed_attrs.merge!(k[0..-5].to_sym => v)
+              end
+            end
           end
 
           attributes = { package: transformed_attrs }
