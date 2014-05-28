ad30240e73701db69e2b08bbf80bae8f549822fb
Washington Luiz <huoxito@gmail.com>
2014-05-28 14:53:47 -0300

    Fix Customer field references and + payment_method to SalesOrder
    See https://system.netsuite.com/help/helpcenter/en_US/srbrowser/Browser2014_1/schema/record/customer.html

--- lib/netsuite/records/customer.rb
+++ lib/netsuite/records/customer.rb
@@ -9,19 +9,22 @@ module NetSuite
 
       actions :get, :get_list, :add, :update, :upsert, :delete, :search
 
+      # See https://system.netsuite.com/help/helpcenter/en_US/srbrowser/Browser2014_1/schema/record/customer.html
+      # for the complete list of attributes their descriptions
+
       fields :access_role, :account_number, :aging, :alt_email, :alt_name, :alt_phone, :bill_pay,
-        :buying_reason, :buying_time_frame, :campaign_category, :category, :click_stream, :comments, :company_name,
+        :buying_reason, :buying_time_frame, :campaign_category, :click_stream, :comments, :company_name,
         :consol_aging, :consol_days_overdue, :contact_roles_list, :contrib_pct, :credit_cards_list, :credit_hold_override,
         :credit_limit, :currency, :currency_list, :date_created, :days_overdue, :default_address,
         :download_list, :email, :email_preference, :email_transactions, :end_date, :entity_id,
         :estimated_budget, :fax, :fax_transactions, :first_name, :first_visit, :give_access, :global_subscription_status,
         :group_pricing_list, :home_phone, :image, :is_budget_approved, :is_inactive, :is_person, :item_pricing_list, :keywords,
-        :language, :last_modified_date, :last_name, :last_page_visited, :last_visit, :lead_source, :middle_name, :mobile_phone,
+        :language, :last_modified_date, :last_name, :last_page_visited, :last_visit, :middle_name, :mobile_phone,
         :opening_balance, :opening_balance_account, :opening_balance_date, :parent, :partners_list,
-        :password, :password2, :phone, :phonetic_name, :pref_cc_processor, :price_level, :print_on_check_as,
+        :password, :password2, :phone, :phonetic_name, :pref_cc_processor, :print_on_check_as,
         :print_transactions, :referrer, :reminder_days, :representing_subsidiary, :require_pwd_change, :resale_number,
         :sales_group, :sales_readiness, :sales_rep, :sales_team_list, :salutation, :send_email, :ship_complete, :shipping_item,
-        :stage, :start_date, :subscriptions_list, :subsidiary, :sync_partner_teams, :tax_exempt, :tax_item, :taxable, :terms,
+        :stage, :start_date, :subscriptions_list, :sync_partner_teams, :tax_exempt, :tax_item, :taxable, :terms,
         :territory, :third_party_acct, :third_party_country, :third_party_zipcode, :title, :url,
         :vat_reg_number, :visits, :web_lead
 
@@ -31,7 +34,8 @@ module NetSuite
       read_only_fields :balance, :consol_balance, :deposit_balance, :consol_deposit_balance, :overdue_balance,
         :consol_overdue_balance, :unbilled_orders, :consol_unbilled_orders
 
-      record_refs :custom_form, :entity_status, :partner
+      record_refs :custom_form, :entity_status, :partner, :category, :lead_source,
+        :price_level, :subsidiary
 
       attr_reader   :internal_id
       attr_accessor :external_id
@@ -50,7 +54,6 @@ module NetSuite
         end
         rec
       end
-
     end
   end
 end
--- spec/netsuite/records/customer_spec.rb
+++ spec/netsuite/records/customer_spec.rb
@@ -6,19 +6,19 @@ describe NetSuite::Records::Customer do
   it 'has all the right fields' do
     [
       :access_role, :account_number, :aging, :alt_email, :alt_name, :alt_phone, :balance, :bill_pay,
-      :buying_reason, :buying_time_frame, :campaign_category, :category, :click_stream, :comments, :company_name,
+      :buying_reason, :buying_time_frame, :campaign_category, :click_stream, :comments, :company_name,
       :consol_aging, :consol_balance, :consol_days_overdue, :consol_deposit_balance, :consol_overdue_balance,
       :consol_unbilled_orders, :contact_roles_list, :contrib_pct, :credit_cards_list, :credit_hold_override, :credit_limit,
       :currency, :currency_list, :date_created, :days_overdue, :default_address,
       :deposit_balance, :download_list, :email, :email_preference, :email_transactions, :end_date, :entity_id,
       :estimated_budget, :fax, :fax_transactions, :first_name, :first_visit, :give_access, :global_subscription_status,
       :group_pricing_list, :home_phone, :image, :is_budget_approved, :is_inactive, :is_person, :item_pricing_list, :keywords,
-      :language, :last_modified_date, :last_name, :last_page_visited, :last_visit, :lead_source, :middle_name, :mobile_phone,
+      :language, :last_modified_date, :last_name, :last_page_visited, :last_visit, :middle_name, :mobile_phone,
       :opening_balance, :opening_balance_account, :opening_balance_date, :overdue_balance, :parent, :partners_list,
-      :password, :password2, :phone, :phonetic_name, :pref_cc_processor, :price_level, :print_on_check_as,
+      :password, :password2, :phone, :phonetic_name, :pref_cc_processor,:print_on_check_as,
       :print_transactions, :referrer, :reminder_days, :representing_subsidiary, :require_pwd_change, :resale_number,
       :sales_group, :sales_readiness, :sales_rep, :sales_team_list, :salutation, :send_email, :ship_complete, :shipping_item,
-      :stage, :start_date, :subscriptions_list, :subsidiary, :sync_partner_teams, :tax_exempt, :tax_item, :taxable, :terms,
+      :stage, :start_date, :subscriptions_list, :sync_partner_teams, :tax_exempt, :tax_item, :taxable, :terms,
       :territory, :third_party_acct, :third_party_country, :third_party_zipcode, :title, :unbilled_orders, :url,
       :vat_reg_number, :visits, :web_lead
     ].each do |field|
