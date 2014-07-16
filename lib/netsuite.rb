c76e4257ad43667087103181f2c8d598e8fab72d
John Hirbour <john@hirbour.org>
2014-07-16 10:16:46 -0400

    added get and search for campaigns
    Conflicts:
    	lib/netsuite.rb

--- lib/netsuite.rb
+++ lib/netsuite.rb
@@ -59,6 +59,7 @@ module NetSuite
     autoload :CashRefund,                 'netsuite/records/cash_refund'
     autoload :CashRefundItem,             'netsuite/records/cash_refund_item'
     autoload :CashRefundItemList,         'netsuite/records/cash_refund_item_list'
+    autoload :Campaign,                   'netsuite/records/campaign'
     autoload :Classification,             'netsuite/records/classification'
     autoload :CreditMemo,                 'netsuite/records/credit_memo'
     autoload :CreditMemoApply,            'netsuite/records/credit_memo_apply'
