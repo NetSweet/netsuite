2fc228bdbe37424414785e574cdc6afc3c4f4a28
Kyle Conroy <kjc@stripe.com>
2014-09-29 20:21:30 -0700

    Add total field to cash refund

--- lib/netsuite/records/cash_refund.rb
+++ lib/netsuite/records/cash_refund.rb
@@ -9,7 +9,7 @@ module NetSuite
 
       actions :add, :get, :delete, :initialize, :upsert
 
-      fields :tran_date, :to_be_emailed
+      fields :tran_date, :to_be_emailed, :total
 
       field :item_list, CashRefundItemList
       field :custom_field_list, CustomFieldList
