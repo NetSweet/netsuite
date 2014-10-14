9b73dd8dee6cb07b6f989997d4e5de6845c28815
Kyle Conroy <kjc@stripe.com>
2014-10-14 15:39:58 -0700

    Add search to location

--- lib/netsuite/records/location.rb
+++ lib/netsuite/records/location.rb
@@ -5,7 +5,7 @@ module NetSuite
       include Support::RecordRefs
       include Support::Actions
 
-      actions :get
+      actions :get, :search
 
       fields :addr1, :addr2, :addr3, :addr_phone, :addr_text, :addressee, :attention, :city, :country, :include_children,
         :is_inactive, :make_inventory_available, :make_inventory_available_store, :name, :override, :state, :tran_prefix, :zip
