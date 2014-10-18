f6eca7e0aaea3836e6fc97d5437876b5202731f8
Greggory Rothmeier <greggroth@gmail.com>
2014-10-18 11:16:12 -0700

    Add reader method for internal_id

--- lib/netsuite/records/custom_list.rb
+++ lib/netsuite/records/custom_list.rb
@@ -14,12 +14,14 @@ module NetSuite
 
       field :custom_value_list, NetSuite::Support::GenericList
 
+      attr_reader :internal_id
+      attr_accessor :external_id
+
       def initialize(attributes = {})
         @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
         @external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
         initialize_from_attributes_hash(attributes)
       end
-      
     end
   end
 end
