abce6803fc6c3e28c29349fd749a477ad3ddf204
Michael Bianco <iloveitaly@gmail.com>
2014-11-22 15:54:53 -0500

    Adding pry for easiest development

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..') ))
 
 require 'rspec'
 require 'netsuite'
+require 'pry'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
