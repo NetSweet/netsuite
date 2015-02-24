62534b5dc98c79f090b98bf93a3e067f3072870f
Eric Collins <eric@thoughtbot.com>
2015-02-24 17:33:08 -0500

    Add employee information

--- lib/netsuite/records/employee.rb
+++ lib/netsuite/records/employee.rb
@@ -1,6 +1,7 @@
 module NetSuite
   module Records
     class Employee < Support::Base
+      include Support::RecordRefs
       include Support::Actions
       include Support::RecordRefs
       include Namespaces::ListEmp
@@ -9,11 +10,15 @@ module NetSuite
 
       actions :get, :get_list, :add, :update, :upsert, :upsert_list, :delete, :search
 
-      fields :phone, :first_name, :last_name, :is_inactive, :email, :give_access, :send_email,
-             :is_support_rep, :birth_date, :hire_date, :last_review_date, :next_review_date, :title,
-             :home_phone, :office_phone, :eligible_for_commission, :is_sales_rep, :klass, :middle_name
+      fields :phone, :first_name, :last_name, :is_inactive, :email, :give_access, :send_email, :is_support_rep,
+             :birth_date, :hire_date, :last_review_date, :next_review_date, :title, :home_phone, :office_phone,
+             :eligible_for_commission, :is_sales_rep, :klass, :middle_name, :account_number, :alien_number, :approval_limit,
+             :bill_pay, :comments, :date_created, :direct_deposit, :employee_status, :employee_type, :entity_id,
+             :expense_limit, :fax, :is_job_resource, :job_description, :labor_cost, :last_modified_date, :mobile_phone, :pay_frequency,
+             :phonetic_name, :purchase_order_approval_limit, :purchase_order_approver, :purchase_order_limit, :release_date,
+             :resident_status, :salutation, :social_security_number, :visa_exp_date, :visa_type
 
-      record_refs :location
+      record_refs :currency, :department, :location, :subsidiary
 
       field :roles_list, RoleList
 
--- spec/netsuite/records/employee_spec.rb
+++ spec/netsuite/records/employee_spec.rb
@@ -5,8 +5,15 @@ describe NetSuite::Records::Employee do
 
   it 'has all the right fields' do
     [
-      :email, :first_name, :give_access, :is_inactive, :is_sales_rep,
-      :klass, :last_name, :middle_name, :phone, :send_email
+      :account_number, :alien_number, :approval_limit, :bill_pay, :birth_date,
+      :comments, :date_created, :direct_deposit, :eligible_for_commission,
+      :email, :employee_status, :employee_type, :entity_id, :expense_limit,
+      :fax, :first_name, :hire_date, :home_phone, :is_inactive, :is_job_resource,
+      :job_description, :labor_cost, :last_modified_date, :last_name, :middle_name,
+      :mobile_phone, :next_review_date, :office_phone, :pay_frequency, :phone,
+      :phonetic_name, :purchase_order_approval_limit, :purchase_order_approver,
+      :purchase_order_limit, :release_date, :resident_status, :salutation,
+      :social_security_number, :title, :visa_exp_date, :visa_type
     ].each do |field|
       expect(employee).to have_field(field)
     end
@@ -132,6 +139,14 @@ describe NetSuite::Records::Employee do
   describe '#record_type' do
     it 'returns a string type for the record to be used in a SOAP request' do
       expect(employee.record_type).to eql('listEmp:Employee')
+    end
+  end
+
+  it 'has the right record_refs' do
+    [
+      :currency, :department, :location, :subsidiary
+    ].each do |record_ref|
+      expect(employee).to have_record_ref(record_ref)
     end
   end
 end
