ca404c2dd7e13c4a6c3004a6795815fc11d2c33c
Eric Collins <eric@thoughtbot.com>
2015-02-24 11:43:07 -0500

    Make test suite run without failing tests

--- lib/netsuite/records/payment_method.rb
+++ lib/netsuite/records/payment_method.rb
@@ -5,8 +5,7 @@ module NetSuite
       include Support::RecordRefs
       include Support::Actions
 
-      actions :add, :delete, :get, :get_list, :search, :search_more_with_id,
-        :update, :upsert, :upsert_list
+      actions :add, :delete, :get, :get_list, :search, :update, :upsert, :upsert_list
 
       fields :credit_card, :express_checkout_arrangement, :is_debit_card, :is_inactive, :is_online, :name,
         :pay_pal_email_address, :undep_funds, :use_express_checkout
