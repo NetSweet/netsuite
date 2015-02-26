5deb4bd8962e16a237b18cb00e3a72146892ebc8
Jeremy D. Frens <jdfrens@gmail.com>
2015-02-26 14:34:26 -0600

    Add fields to Employee; start specs.

--- lib/netsuite/records/employee.rb
+++ lib/netsuite/records/employee.rb
@@ -1,20 +1,23 @@
 module NetSuite
   module Records
-
     class Employee < Support::Base
+      include Support::Fields
       include Support::Actions
+      include Support::RecordRefs
       include Namespaces::ListEmp
 
       # https://system.netsuite.com/help/helpcenter/en_US/srbrowser/Browser2014_1/script/record/employee.html
 
       actions :get, :get_list, :add, :update, :upsert, :upsert_list, :delete, :search
 
-      fields :phone, :first_name, :last_name, :is_inactive, :email
+      fields :email, :first_name, :give_access, :is_inactive, :is_sales_rep,
+             :klass, :last_name, :middle_name, :phone, :send_email
+
+      record_refs :location
 
       field :roles_list, RoleList
 
-      attr_reader   :internal_id
+      attr_reader :internal_id
     end
-
   end
 end
