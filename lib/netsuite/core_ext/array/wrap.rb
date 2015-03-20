4bbca1647f1be36c8d6483bfc7b61707c0b29b0e
Jeremy Croteau <jeremy.croteau@gmail.com>
2015-03-19 21:50:16 -0400

    Add 'Array.wrap' core extension, adopted from Rails::ActiveSupport

---
+++ lib/netsuite/core_ext/array/wrap.rb
@@ -0,0 +1,11 @@
+class Array
+  def self.wrap(object)
+    if object.nil?
+      []
+    elsif object.respond_to?(:to_ary)
+      object.to_ary || [object]
+    else
+      [object]
+    end
+  end
+end
