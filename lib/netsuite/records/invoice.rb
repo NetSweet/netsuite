688a94a816afc75e4c145a8a5be92c528ac50ae7
Michael Bianco <iloveitaly@gmail.com>
2015-04-19 10:09:54 -0400

    Fixing invoice promo code reference

--- lib/netsuite/records/invoice.rb
+++ lib/netsuite/records/invoice.rb
@@ -20,7 +20,7 @@ module NetSuite
         :item_cost_disc_rate, :item_cost_disc_tax_1_amt, :item_cost_disc_taxable, :item_cost_discount, :item_cost_list,
         :item_cost_tax_code, :item_cost_tax_rate_1, :item_cost_tax_rate_2, :item_list, :job, :last_modified_date,
         :linked_tracking_numbers, :memo, :message, :message_sel, :on_credit_hold, :opportunity,
-        :other_ref_num, :partners_list, :promo_code, :rev_rec_end_date,
+        :other_ref_num, :partners_list, :rev_rec_end_date,
         :rev_rec_on_rev_commitment, :rev_rec_schedule, :rev_rec_start_date, :revenue_status, :sales_effective_date,
         :sales_group, :sales_team_list, :ship_address, :ship_date, :ship_group_list,
         :shipping_cost, :shipping_tax_1_rate, :shipping_tax_2_rate, :shipping_tax_code, :source, :start_date,
@@ -40,7 +40,7 @@ module NetSuite
 
       record_refs :account, :bill_address_list, :custom_form, :department, :entity, :klass, :partner,
                   :posting_period, :ship_address_list, :terms, :location, :sales_rep, :tax_item, :created_from,
-                  :ship_method, :lead_source
+                  :ship_method, :lead_source, :promo_code
 
       attr_reader   :internal_id
       attr_accessor :external_id
