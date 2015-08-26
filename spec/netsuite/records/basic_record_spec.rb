c3724865e6c90af082b00c809a3bae116db32077
Michael Bianco <iloveitaly@gmail.com>
2015-08-26 09:07:38 -0400

    Removing currency spec, using generic record spec instead
    I don't think the existing specs add that much value. They are a pain to write and don't tell us much.
    
    I'm testing out a simplier "basic" record spec. If this passes, there is a good chance the NS gem logic will work fine
    in the wild, although it's no gaurentee that the logic lines up with what NetSuite returns.

---
+++ spec/netsuite/records/basic_record_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe 'basic records' do
+  let(:basic_record_list) {
+    [
+      NetSuite::Records::Currency,
+      NetSuite::Records::Location
+    ]
+  }
+
+  it 'can be initialized, has fields, and can be converted into a record' do
+    basic_record_list.each do |record_class|
+      record_instance = record_class.new
+
+      # most records have a
+      expect(record_class.fields.size).to be > 0
+      expect(record_class.record_refs).to_not be_nil
+      expect(record_instance).to respond_to(:internal_id)
+
+      # NOTE all records have externalIds, not testing exernalId setter
+      if record_class.respond_to?(:upsert)
+        expect(record_instance).to respond_to(:external_id)
+        expect(record_instance).to respond_to(:external_id=)
+      end
+
+      # ensure that all fields can be set
+      standard_fields = (record_class.fields - record_class.record_refs).to_a
+
+      standard_fields.each { |f| expect(record_instance).to have_field(f) }
+      record_class.record_refs.each { |f| expect(record_instance).to have_record_ref(f) }
+
+      6.times do
+        record_instance.send(:"#{standard_fields.sample}=", "Test Value")
+      end
+
+      if !record_class.record_refs.empty?
+        sample_record_ref_field = record_class.record_refs.to_a.sample
+
+        record_instance.send(:"#{sample_record_ref_field}=".to_sym, { internal_id: 1 })
+      end
+
+      expect(record_instance.to_record).to be_a(Hash)
+
+      # TODO should test for correct value output
+    end
+  end
+end
