61775400696e30537a1cb019277843aac30c48b7
berkcaputcu <berk.caputcu@shopify.com>
2015-10-28 20:54:47 +0000

    use sublist on customer refund apply list

--- lib/netsuite/records/customer_refund_apply_list.rb
+++ lib/netsuite/records/customer_refund_apply_list.rb
@@ -1,32 +1,15 @@
 module NetSuite
   module Records
-    class CustomerRefundApplyList
-      include Support::Fields
+    class CustomerRefundApplyList < Support::Sublist
       include Namespaces::TranCust
 
-      fields :apply
+      sublist :apply, CustomerRefundApply
 
-      def initialize(attributes = {})
-        initialize_from_attributes_hash(attributes)
-      end
-
-      def apply=(applies)
-        case applies
-          when Hash
-            self.applies << CustomerRefundApply.new(applies)
-          when Array
-            applies.each { |apply| self.applies << CustomerRefundApply.new(apply) }
-        end
-      end
-
+      # for backward compatibility
       def applies
-        @applies ||= []
+        self.apply
       end
-
-      def to_record
-        { "#{record_namespace}:apply" => applies.map(&:to_record) }
-      end
-
     end
+
   end
 end
