48ebbb86e9f84ab406b01fc3979785d3b119f958
John Perkins <jeperkins4@gmail.com>
2016-04-08 12:29:31 -0400

    Adding price to CashRefundItem

--- lib/netsuite/records/cash_refund_item.rb
+++ lib/netsuite/records/cash_refund_item.rb
@@ -8,7 +8,7 @@ module NetSuite
 
       field :amount, :rate
 
-      record_refs :item, :klass
+      record_refs :item, :klass, :price
 
       def initialize(attributes_or_record = {})
         initialize_from_attributes_hash(attributes_or_record)
