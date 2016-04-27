28e3d736078431ed819a3ee337eaafeb4246bef7
Michael Bianco <iloveitaly@gmail.com>
2016-04-26 22:11:47 -0400

    Adding missing quantity sales order item fields

--- lib/netsuite/records/sales_order_item.rb
+++ lib/netsuite/records/sales_order_item.rb
@@ -13,7 +13,7 @@ module NetSuite
         :gift_cert_message, :gift_cert_number, :gift_cert_recipient_email, :gift_cert_recipient_name, :gross_amt, :is_taxable,
         :line, :order_line, :po_currency, :quantity, :rate, :rev_rec_end_date, :rev_rec_start_date, :rev_rec_term_in_months,
         :serial_numbers, :shipping_cost, :tax1_amt, :tax_rate1, :tax_rate2, :vsoe_allocation, :vsoe_amount, :vsoe_deferral,
-        :vsoe_delivered, :vsoe_permit_discount, :vsoe_price, :is_closed
+        :vsoe_delivered, :vsoe_permit_discount, :vsoe_price, :is_closed, :quantity_commited, :quantity_fulfilled
 
       field :custom_field_list, CustomFieldList
 
