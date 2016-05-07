a3515f7e16dd973d264e96dcbddf021f3c630156
Michael Bianco <iloveitaly@gmail.com>
2016-05-07 17:44:23 -0400

    Removing use of Support::Base

--- lib/netsuite/records/file.rb
+++ lib/netsuite/records/file.rb
@@ -1,6 +1,8 @@
 module NetSuite
   module Records
-    class File < NetSuite::Support::Base
+    class File
+      include Support::Records
+      include Support::Fields
       include Support::RecordRefs
       include Support::Actions
       include Namespaces::FileCabinet
@@ -17,7 +19,13 @@ module NetSuite
 
       attr_reader   :internal_id
       attr_accessor :external_id
+
+      def initialize(attributes = {})
+        @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
+        @external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
+        initialize_from_attributes_hash(attributes)
+      end
+
     end
   end
 end
-
