78c59ebda537d48cc8c5538c28cc20bb91399339
Sarah Anderson <seanderson07@gmail.com>
2016-07-23 15:11:15 -0400

    Adds GiftCertRedemptionList and GiftCertRedemption records classes. Adds the :gift_cert_redemption_list field to the sales order records class.

---
+++ lib/netsuite/records/gift_cert_redemption.rb
@@ -0,0 +1,24 @@
+module NetSuite
+  module Records
+    class GiftCertRedemption
+      include Support::Fields
+      include Support::RecordRefs
+      include Support::Records
+      include Support::Actions
+      include Namespaces::TranSales
+
+      actions :get, :get_list, :add, :initialize, :delete, :update, :upsert, :search
+
+      fields :auth_code_amt_remaining, :auth_code_applied, :gift_cert_available
+
+      record_refs :auth_code
+
+      attr_reader :internal_id
+
+      def initialize(attributes = {})
+        initialize_from_attributes_hash(attributes)
+      end
+
+    end
+  end
+end
