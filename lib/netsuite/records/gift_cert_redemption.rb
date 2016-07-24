9c4a2c4564324ee0662bd3e7eda291cffcdbfa63
Sarah Anderson <seanderson07@gmail.com>
2016-07-24 11:36:37 -0400

    Adds manually setting the internal_id.

--- lib/netsuite/records/gift_cert_redemption.rb
+++ lib/netsuite/records/gift_cert_redemption.rb
@@ -16,6 +16,7 @@ module NetSuite
       attr_reader :internal_id
 
       def initialize(attributes = {})
+        @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
         initialize_from_attributes_hash(attributes)
       end
 
