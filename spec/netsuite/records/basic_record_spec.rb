a0f14e068d296fa234239967b43efb78f0478335
Sarah Anderson <seanderson07@gmail.com>
2016-07-30 21:50:58 -0400

    Removes NetSuite::Records::GiftCertRedemption from the basic_record_list since it does not have an internal_id.

--- spec/netsuite/records/basic_record_spec.rb
+++ spec/netsuite/records/basic_record_spec.rb
@@ -34,7 +34,6 @@ describe 'basic records' do
       NetSuite::Records::ContactRole,
       NetSuite::Records::PaymentItem,
       NetSuite::Records::CreditMemo,
-      NetSuite::Records::GiftCertRedemption,
       NetSuite::Records::InventoryItem,
     ]
   }
