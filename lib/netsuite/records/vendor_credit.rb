5669e2729fe7449859c6342befde3aa83ab4ba1f
David Laprade <david.laprade@gmail.com>
2016-08-12 08:56:27 -0400

    Add support for VendorCredit records

---
+++ lib/netsuite/records/vendor_credit.rb
@@ -0,0 +1,52 @@
+module NetSuite
+  module Records
+    class VendorCredit
+      include Support::Fields
+      include Support::RecordRefs
+      include Support::Records
+      include Support::Actions
+      include Namespaces::TranPurch
+
+      actions :get, :get_list, :delete, :initialize, :search
+
+      fields :created_date,  :un_applied, :last_modified_date,
+             :auto_apply,    :applied,    :transaction_number,
+             :tran_id,       :total,      :user_total,
+             :currency_name, :tran_date,  :exchange_rate,
+             :memo
+
+      field :custom_form,        RecordRef
+      field :account,            RecordRef
+      field :bill_address_list,  RecordRef
+      field :created_from,       RecordRef
+      field :entity,             RecordRef
+      field :currency,           RecordRef
+      field :posting_period,     RecordRef
+      field :department,         RecordRef
+      field :klass,              RecordRef
+      field :location,           RecordRef
+      field :subsidiary,         RecordRef
+      field :billing_address,    Address
+      # field :expenseList,        VendorCreditExpenseList
+      # field :itemList,           VendorCreditItemList
+      # field :applyList,          VendorCreditApplyList
+      field :custom_field_list,  CustomFieldList
+
+      attr_reader :internal_id
+
+      def initialize(attributes = {})
+        @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
+        initialize_from_attributes_hash(attributes)
+      end
+
+      def self.search_class_name
+        "Transaction"
+      end
+
+      def self.search_class_namespace
+        'tranSales'
+      end
+
+    end
+  end
+end
--- spec/netsuite/records/basic_record_spec.rb
+++ spec/netsuite/records/basic_record_spec.rb
@@ -12,6 +12,7 @@ describe 'basic records' do
       NetSuite::Records::PayrollItem,
       NetSuite::Records::Opportunity,
       NetSuite::Records::VendorCategory,
+      NetSuite::Records::VendorCredit,
       NetSuite::Records::VendorBill,
       NetSuite::Records::Deposit,
       NetSuite::Records::RevRecTemplate,
