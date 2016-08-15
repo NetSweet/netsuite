ce49a44ec2b597a22787fbae4c2585ce023cdee0
David Laprade <david.laprade@gmail.com>
2016-08-15 12:48:46 -0400

    Refactor CustomerRefundApplyList to use Support::Sublist

--- lib/netsuite/records/customer_refund_apply_list.rb
+++ lib/netsuite/records/customer_refund_apply_list.rb
@@ -1,30 +1,13 @@
 module NetSuite
   module Records
-    class CustomerRefundApplyList
+    class CustomerRefundApplyList < Support::Sublist
       include Namespaces::TranCust
-
-      # TODO should use new sublist implementation
 
       attr_accessor :replace_all
 
-      def initialize(attributes = {})
-        case attributes[:apply]
-        when Hash
-          applies << CustomerRefundApply.new(attributes[:apply])
-        when Array
-          attributes[:apply].each { |apply| applies << CustomerRefundApply.new(apply) }
-        end
-      end
+      sublist :apply, CustomerRefundApply
 
-      def applies
-        @applies ||= []
-      end
-
-      def to_record
-        rec = { "#{record_namespace}:apply" => applies.map(&:to_record) }
-        rec[:@replaceAll] = @replace_all unless @replace_all.nil?
-        rec
-      end
+      alias :applies :apply
 
     end
   end
