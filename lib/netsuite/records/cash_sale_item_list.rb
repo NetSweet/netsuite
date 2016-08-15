6563c4ceaecb2eb6dc61ea813525e215b9c4575a
David Laprade <david.laprade@gmail.com>
2016-08-15 12:54:55 -0400

    Refactor CashSaleItemList to use Support::Sublist

--- lib/netsuite/records/cash_sale_item_list.rb
+++ lib/netsuite/records/cash_sale_item_list.rb
@@ -1,24 +1,12 @@
 module NetSuite
   module Records
-    class CashSaleItemList
+    class CashSaleItemList < Support::Sublist
       include Namespaces::TranSales
 
-      def initialize(attributes = {})
-        case attributes[:item]
-        when Hash
-          items << CashSaleItem.new(attributes[:item])
-        when Array
-          attributes[:item].each { |item| items << CashSaleItem.new(item) }
-        end
-      end
+      sublist :item, CashSaleItem
 
-      def items
-        @items ||= []
-      end
+      alias :items :item
 
-      def to_record
-        { "#{record_namespace}:item" => items.map(&:to_record) }
-      end
     end
   end
 end
