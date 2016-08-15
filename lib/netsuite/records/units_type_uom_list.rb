af99af7d268832c908a128f9848d94a63af99b05
David Laprade <david.laprade@gmail.com>
2016-08-15 14:07:57 -0400

    Refactor UnitsTypeUomList to use Support::Sublist

--- lib/netsuite/records/units_type_uom_list.rb
+++ lib/netsuite/records/units_type_uom_list.rb
@@ -1,31 +1,10 @@
 module NetSuite
   module Records
-    class UnitsTypeUomList
-      include Support::Fields
+    class UnitsTypeUomList < Support::Sublist
       include Namespaces::ListAcct
 
-      fields :uom
+      sublist :uom, UnitsTypeUom
 
-      def initialize(attributes = {})
-        initialize_from_attributes_hash(attributes)
-      end
-
-      def uom=(items)
-        case items
-        when Hash
-          self.uom << UnitsTypeUom.new(items)
-        when Array
-          items.each { |item| self.uom << UnitsTypeUom.new(item) }
-        end
-      end
-
-      def uom
-        @uom ||= []
-      end
-
-      def to_record
-        { "#{record_namespace}:uom" => uom.map(&:to_record) }
-      end
     end
   end
 end
