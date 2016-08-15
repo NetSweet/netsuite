2faf4eb9c62a5233d19ea3ce807684dadc66f14f
David Laprade <david.laprade@gmail.com>
2016-08-15 13:38:19 -0400

    Refactor JournalEntryLineList to use Support::Sublist

--- lib/netsuite/records/journal_entry_line_list.rb
+++ lib/netsuite/records/journal_entry_line_list.rb
@@ -1,31 +1,11 @@
 module NetSuite
   module Records
-    class JournalEntryLineList
-      include Support::Fields
+    class JournalEntryLineList < Support::Sublist
       include Namespaces::TranGeneral
-      
-      fields :line
 
-      def initialize(attributes = {})
-        initialize_from_attributes_hash(attributes)
-      end
+      sublist :line, JournalEntryLine
 
-      def line=(lines)
-        case lines
-        when Hash
-          self.lines << JournalEntryLine.new(lines)
-        when Array
-          lines.each { |line| self.lines << JournalEntryLine.new(line) }
-        end
-      end
-
-      def lines
-        @items ||= []
-      end
-
-      def to_record
-        { "#{record_namespace}:line" => lines.map(&:to_record) }
-      end
+      alias :lines :line
 
     end
   end
