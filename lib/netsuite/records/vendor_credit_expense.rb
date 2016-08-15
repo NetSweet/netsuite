e0c56b60279f78a9c6a7b111e214466c2b946e78
David Laprade <david.laprade@gmail.com>
2016-08-14 20:35:05 -0400

    Add support for VendorCreditExpense records

---
+++ lib/netsuite/records/vendor_credit_expense.rb
@@ -0,0 +1,30 @@
+module NetSuite
+  module Records
+    class VendorCreditExpense
+      include Support::Fields
+      include Namespaces::TranPurch
+
+      fields :order_line,  :line,      :amount,
+             :tax_rate1,   :tax_rate2, :tax1_amt,
+             :gross_amt,   :memo,      :is_billable,
+             :amortiz_start_date,
+             :amortization_end_date,
+             :amortization_residual
+
+      field :category,            RecordRef
+      field :taxCode,             RecordRef
+      field :account,             RecordRef
+      field :department,          RecordRef
+      field :klass,               RecordRef
+      field :amortizationSched,   RecordRef
+      field :location,            RecordRef
+      field :customer,            RecordRef
+      field :custom_field_list,   CustomFieldList
+
+      def initialize(attributes = {})
+        initialize_from_attributes_hash(attributes)
+      end
+
+    end
+  end
+end
