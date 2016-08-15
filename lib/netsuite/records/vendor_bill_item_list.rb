a4138208afad000d5f2c54234a011a06416f05fb
David Laprade <david.laprade@gmail.com>
2016-08-15 13:58:30 -0400

    Refactor VendorBillItemList to use Support::Sublist

--- lib/netsuite/records/vendor_bill_item_list.rb
+++ lib/netsuite/records/vendor_bill_item_list.rb
@@ -1,31 +1,11 @@
 module NetSuite
   module Records
-    class VendorBillItemList
-      include Support::Fields
+    class VendorBillItemList < Support::Sublist
       include Namespaces::TranPurch
 
-      fields :item
+      sublist :item, VendorBillItem
 
-      def initialize(attributes = {})
-        initialize_from_attributes_hash(attributes)
-      end
-
-      def item=(items)
-        case items
-        when Hash
-          self.items << VendorBillItem.new(items)
-        when Array
-          items.each { |item| self.items << VendorBillItem.new(item) }
-        end
-      end
-
-      def items
-        @items ||= []
-      end
-
-      def to_record
-        { "#{record_namespace}:item" => items.map(&:to_record) }
-      end
+      alias :items :item
 
     end
   end
