6b0b4862768ceae467f29815922725361bb6188f
David Laprade <david.laprade@gmail.com>
2016-08-15 12:55:45 -0400

    Update InventoryTransferInventoryList to use Support::Sublist

--- lib/netsuite/records/inventory_transfer_inventory_list.rb
+++ lib/netsuite/records/inventory_transfer_inventory_list.rb
@@ -1,33 +1,12 @@
 module NetSuite
   module Records
-    class InventoryTransferInventoryList
+    class InventoryTransferInventoryList < Support::Sublist
       include Support::RecordRefs
       include Support::Records
-      include Support::Fields
       include Namespaces::TranInvt
 
-      fields :inventory
+      sublist :inventory, InventoryTransferInventory
 
-      def initialize(attributes = {})
-        initialize_from_attributes_hash(attributes)
-      end
-
-      def inventory=(items)
-        case items
-        when Hash
-          self.inventory << InventoryTransferInventory.new(items)
-        when Array
-          items.each { |item| self.inventory << InventoryTransferInventory.new(item) }
-        end
-      end
-
-      def inventory
-        @inventory ||= []
-      end
-
-      def to_record
-        { "#{record_namespace}:inventory" => inventory.map(&:to_record) }
-      end
     end
   end
 end
