459e19ff59f6ff35124c1d75cc4fc726390462ad
rudyyazdi <rudyyazdi@gmail.com>
2016-08-19 09:16:01 +1000

    refactored

--- lib/netsuite/support/search_result.rb
+++ lib/netsuite/support/search_result.rb
@@ -6,6 +6,8 @@ module NetSuite
       attr_reader :total_records
       attr_reader :total_pages
       attr_reader :current_page
+      attr_reader :response
+      attr_reader :result_class
 
       # header from a basic customer search:
 
@@ -18,58 +20,16 @@ module NetSuite
       #   <platformCore:searchId>WEBSERVICES_738944_SB2_03012013650784545962753432_28d96bd280</platformCore:searchId>
 
       def initialize(response, result_class)
-        @result_class = result_class
-        @response = response
-        
+        @result_class  = result_class
+        @response      = response
         @total_records = response.body[:total_records].to_i
-        @total_pages = response.body[:total_pages].to_i
-        @current_page = response.body[:page_index].to_i
+        @total_pages   = response.body[:total_pages].to_i
+        @current_page  = response.body[:page_index].to_i
 
-        if @total_records > 0
-          if response.body.has_key?(:record_list)
-            # basic search results
-            record_list = response.body[:record_list][:record]
-            record_list = [record_list] unless record_list.is_a?(Array)
-
-            record_list.each do |record|
-              results << result_class.new(record)
-            end
-          elsif response.body.has_key? :search_row_list
-            # advanced search results
-            record_list = response.body[:search_row_list][:search_row]
-            record_list = [record_list] unless record_list.is_a?(Array)
-
-            record_list.each do |record|
-              # TODO because of customFieldList we need to either make this recursive
-              #      or handle the customFieldList as a special case
-
-              record.each_pair do |search_group, search_data|
-                # skip all attributes: look for :basic and all :xxx_join
-                next if search_group.to_s.start_with?('@')
-
-                record[search_group].each_pair do |k, v|
-                  # all return values are wrapped in a <SearchValue/>
-                  # extract the value from <SearchValue/> to make results easier to work with
-
-                  if v.is_a?(Hash) && v.has_key?(:search_value)
-                    # search return values that are just internalIds are stored as attributes on the searchValue element
-                    if v[:search_value].is_a?(Hash) && v[:search_value].has_key?(:'@internal_id')
-                      record[search_group][k] = v[:search_value][:'@internal_id']
-                    else
-                      record[search_group][k] = v[:search_value]
-                    end
-                  end
-                end
-              end
-
-              result_wrapper = result_class.new(record.delete(:basic))
-              result_wrapper.search_joins = record
-              results << result_wrapper
-            end
-          else
-            raise "uncaught search result"
-          end
-        end
+        return unless @total_records > 0
+        return results_from_basic_search    if response.body.key? :record_list
+        return results_from_advanced_search if response.body.key? :search_row_list
+        raise "uncaught search result"
       end
 
       def results
@@ -93,6 +53,60 @@ module NetSuite
         end
 
         yield results
+      end
+
+      private
+
+      # remove the useless :search_value from a hash recursively.
+      # turns this: {:amount_remaining=>{:search_value=>"6030.0"}}
+      # into this: {:amount_remaining=>"6030.0"}
+      # regardless of how deep it is in the hash hierarchy.
+      def remove_search_value(hash)
+        return hash unless hash.is_a? Hash
+        return hash[:search_value] if hash.key? :search_value
+        hash.each { |k, v| hash[k] = remove_search_value(v) }
+      end
+
+      # brings the internal_id to the surface
+      # turns this: {:internal_id=>{:@internal_id=>"5"}}
+      # into this: {:internal_id=>"5"}
+      # regardless of how deep it is in the hash hierarchy.
+      def bring_up_internal_id(hash)
+        return hash unless hash.is_a? Hash
+        if hash.key?(:internal_id) && hash[:internal_id].is_a?(Hash)
+          hash[:internal_id] = hash[:internal_id].values[0]
+          return hash
+        end
+        hash.each { |k, v| hash[k] = bring_up_internal_id(v) }
+      end
+
+      def cleanup_record(record)
+        # TODO because of customFieldList we need to either make this recursive
+        #      or handle the customFieldList as a special case
+
+        search_value_removed_record = remove_search_value(record)
+        bring_up_internal_id(search_value_removed_record)
+      end
+
+      def results_from_basic_search
+        record_list = response.body[:record_list][:record]
+        record_list = [record_list] unless record_list.is_a?(Array)
+
+        record_list.each do |record|
+          results << result_class.new(record)
+        end
+      end
+
+      def results_from_advanced_search
+        record_list = response.body[:search_row_list][:search_row]
+        record_list = [record_list] unless record_list.is_a?(Array)
+
+        record_list.each do |record|
+          clean_record = cleanup_record(record)
+          result_wrapper = result_class.new(clean_record.delete(:basic))
+          result_wrapper.search_joins = clean_record
+          results << result_wrapper
+        end
       end
 
     end
