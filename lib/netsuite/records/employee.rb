62f78fb911b0e6efc274d343d1f319b1838421cc
ellisbenjamin <ellisbe@gmail.com>
2016-08-31 11:15:38 -0400

    trying.

--- lib/netsuite/records/employee.rb
+++ lib/netsuite/records/employee.rb
@@ -33,6 +33,10 @@ module NetSuite
         initialize_from_attributes_hash(attributes)
       end
 
+      def confirm
+        puts "hi"
+      end
+
       def self.search_class_name
         'Employee'
       end
