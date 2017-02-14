0e6f802156f108db5f0dde16ddda2031902035ee
laurafg <laura@camaloon.com>
2017-02-14 13:25:11 +0100

    Adding upsert_list action to invoice, credit_memo, sales_order and item_fulfillment

--- lib/netsuite/records/sales_order.rb
+++ lib/netsuite/records/sales_order.rb
@@ -7,7 +7,7 @@ module NetSuite
       include Support::Actions
       include Namespaces::TranSales
 
-      actions :get, :get_list, :add, :initialize, :delete, :update, :upsert, :search
+      actions :get, :get_list, :add, :initialize, :delete, :update, :upsert, :upsert_list, :search
 
       fields :alt_handling_cost, :alt_shipping_cost, :amount_paid, :amount_remaining, :auto_apply, :balance,
         :bill_address, :cc_approved, :contrib_pct, :created_date, :currency_name, :deferred_revenue, :discount_rate, :email, :end_date,
