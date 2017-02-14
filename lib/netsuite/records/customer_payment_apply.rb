a62b6bb3e8a79c49ea3c803b3ac75bdaee8a7358
Michael Bianco <iloveitaly@gmail.com>
2017-02-14 09:18:14 -0700

    Fixing CustomerPayment formatting

--- lib/netsuite/records/customer_payment_apply.rb
+++ lib/netsuite/records/customer_payment_apply.rb
@@ -2,15 +2,15 @@ module NetSuite
   module Records
   	class CustomerPaymentApply
   		include Support::Fields
-      	include Support::Records
-      	include Namespaces::TranCust
+    	include Support::Records
+    	include Namespaces::TranCust
 
-  		fields :amount, :apply, :apply_date, :currency, :disc, :disc_amt, :disc_date, 
+  		fields :amount, :apply, :apply_date, :currency, :disc, :disc_amt, :disc_date,
   				:doc, :due, :job, :line, :ref_num, :total, :type
 
-		def initialize(attributes = {})
-			initialize_from_attributes_hash(attributes)
-		end
+  		def initialize(attributes = {})
+  			initialize_from_attributes_hash(attributes)
+  		end
 
   	end
   end
