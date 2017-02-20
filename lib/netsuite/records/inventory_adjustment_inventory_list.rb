00fec49223e5dbcc736e4b6357f69155b710e1d1
sherriously <lusherri@gmail.com>
2017-02-20 08:10:22 -0500

    remove redundant support modules in Inventory Adjustment Inventory List

--- lib/netsuite/records/inventory_adjustment_inventory_list.rb
+++ lib/netsuite/records/inventory_adjustment_inventory_list.rb
@@ -1,8 +1,6 @@
 module NetSuite
   module Records
     class InventoryAdjustmentInventoryList < Support::Sublist
-      include Support::RecordRefs
-      include Support::Records
       include Namespaces::TranInvt
 
       sublist :inventory, InventoryAdjustmentInventory
