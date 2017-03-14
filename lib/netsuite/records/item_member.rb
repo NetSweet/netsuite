28863fdd9759fe26258384a73f7fb3c4a33fcb08
Greggory Rothmeier <greggroth@gmail.com>
2017-03-13 18:29:15 -0700

    Add missing reader methods for internal/external ID

--- lib/netsuite/records/item_member.rb
+++ lib/netsuite/records/item_member.rb
@@ -13,7 +13,12 @@ module NetSuite
       record_refs :tax_schedule, :item, :effective_revision,
         :obsolete_revision
 
+      attr_reader :internal_id
+      attr_accessor :external_id
+
       def initialize(attributes = {})
+        @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
+        @external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
         initialize_from_attributes_hash(attributes)
       end
     end
