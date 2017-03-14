d576e6b7edb62d85e3a6df6f855fb44982fc1a26
sherriously <lusherri@gmail.com>
2017-03-14 15:42:06 -0400

    Adding Inventory Number, and Inventory Number locations list

---
+++ lib/netsuite/records/inventory_number_locations.rb
@@ -0,0 +1,15 @@
+module NetSuite
+  module Records
+    class InventoryNumberLocations
+      include Support::Fields
+      include Namespaces::ListAcct
+
+      fields :location, :quantity_available, :quantity_in_transit,
+      :quantity_on_hand, :quantity_on_order
+
+      def initialize(attributes = {})
+        initialize_from_attributes_hash(attributes)
+      end
+    end
+  end
+end
