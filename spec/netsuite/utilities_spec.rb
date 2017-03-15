136383857df2adccdeb672545b0c04fb1210a529
Michael Bianco <iloveitaly@gmail.com>
2017-03-15 07:50:56 -0600

    Spec for handling unexpected errors

--- spec/netsuite/utilities_spec.rb
+++ spec/netsuite/utilities_spec.rb
@@ -32,6 +32,67 @@ describe NetSuite::Utilities do
 
         expect(NetSuite::Records::Account).to have_received(:get).exactly(1).times
       end
+
+      it 'works when an unexpected error occurs' do
+        xml_msg = <<EOL
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <soapenv:Header>
+    <platformMsgs:documentInfo xmlns:platformMsgs="urn:messages_2015_1.platform.webservices.netsuite.com">
+      <platformMsgs:nsId>WEBSERVICES_TSTDRV1022750_103020151182829907744988579_5d1f6b</platformMsgs:nsId>
+    </platformMsgs:documentInfo>
+  </soapenv:Header>
+  <soapenv:Body>
+    <updateResponse xmlns="urn:messages_2015_1.platform.webservices.netsuite.com">
+      <writeResponse>
+        <platformCore:status xmlns:platformCore="urn:core_2015_1.platform.webservices.netsuite.com" isSuccess="false">
+          <platformCore:statusDetail type="ERROR">
+            <platformCore:code>UNEXPECTED_ERROR</platformCore:code>
+            <platformCore:message>An unexpected error occurred. Error ID: igduqcc81p7jpm8fklupr</platformCore:message>
+          </platformCore:statusDetail>
+        </platformCore:status>
+        <baseRef xmlns:platformCore="urn:core_2015_1.platform.webservices.netsuite.com" internalId="50534" type="revRecSchedule" xsi:type="platformCore:RecordRef"/>
+      </writeResponse>
+    </updateResponse>
+  </soapenv:Body>
+</soapenv:Envelope>
+EOL
+        xml_msg = <<EOL
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<soapenv:Body>
+  <soapenv:Fault>
+    <faultcode>soapenv:Server.userException</faultcode>
+    <faultstring>An unexpected error occurred</faultstring>
+    <detail>
+      <ns1:hostname xmlns:ns1="http://xml.apache.org/axis/">partners-java10006.bos.netledger.com</ns1:hostname>
+    </detail>
+  </soapenv:Fault>
+</soapenv:Body>
+EOL
+
+        # <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        #   <soapenv:Body>
+        #     <soapenv:Fault>
+        #       <faultcode>soapenv:Server.userException</faultcode>
+        #       <faultstring>You do not have permission to access web services feature.</faultstring>
+        #       <detail>
+        #         <platformFaults:UnexpectedErrorFault xmlns:platformFaults="urn:faults_2015_1.platform.webservices.netsuite.com">
+        #           <platformFaults:code>UNEXPECTED_ERROR</platformFaults:code>
+        #           <platformFaults:message>An unexpected error occurred. Error ID: j0b0mmf81qhk9h7ezjgbz</platformFaults:message>
+        #         </platformFaults:UnexpectedErrorFault>
+        #         <ns1:hostname xmlns:ns1="http://xml.apache.org/axis/">partners-java10001.bos.netledger.com</ns1:hostname>
+        #       </detail>
+        #     </soapenv:Fault>
+        #   </soapenv:Body>
+        # </soapenv:Envelope>
+
+        ns_account_id = 123
+
+        exception = Savon::SOAPFault.new(OpenStruct.new(body: xml_msg), Nori.new({}), xml_msg)
+
+        allow(NetSuite::Records::Account).to receive(:get).and_raise(exception)
+
+        NetSuite::Utilities.get_record(NetSuite::Records::Account, ns_account_id)
+      end
     end
 
     it 'pulls a record by internal id' do
