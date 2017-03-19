7605db075caf8d92a1105909c576b6d643005080
Sherri <lusherri@gmail.com>
2017-03-19 14:51:27 -0400

    Adding Serialized Inventory Item Numbers

---
+++ lib/netsuite/records/serialized_inventory_item_numbers.rb
@@ -0,0 +1,14 @@
+module NetSuite
+  module Records
+    class SerializedInventoryItemNumbers
+      include Support::Fields
+      include Namespaces::ListAcct
+
+      fields :serial_number
+
+      def initialize(attributes = {})
+        initialize_from_attributes_hash(attributes)
+      end
+    end
+  end
+end
