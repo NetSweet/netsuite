13d4c42f6d7757f65da11bde00e1c740d9b826fa
Sherri <lusherri@gmail.com>
2017-03-20 19:20:34 -0400

    Update Serialized Inventory Item Numbers and Inventory Number Locations to include Support::Records module

--- lib/netsuite/records/inventory_number_locations.rb
+++ lib/netsuite/records/inventory_number_locations.rb
@@ -2,6 +2,7 @@ module NetSuite
   module Records
     class InventoryNumberLocations
       include Support::Fields
+      include Support::Records
       include Namespaces::ListAcct
 
       fields :location, :quantity_available, :quantity_in_transit,
