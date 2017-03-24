755098b9bfa738f530a9904b21eb206b9a0c7cd0
Greggory Rothmeier <greggroth@gmail.com>
2017-03-24 14:12:19 -0700

    Fix the #to_record method on inventory lists

--- lib/netsuite/records/inventory_transfer_inventory_list.rb
+++ lib/netsuite/records/inventory_transfer_inventory_list.rb
@@ -1,8 +1,6 @@
 module NetSuite
   module Records
     class InventoryTransferInventoryList < Support::Sublist
-      include Support::RecordRefs
-      include Support::Records
       include Namespaces::TranInvt
 
       sublist :inventory, InventoryTransferInventory
