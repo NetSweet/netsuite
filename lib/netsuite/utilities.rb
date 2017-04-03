a0cdcfdb5bcd0737fd257fac8a8fd62b8e474c3d
Michael Bianco <iloveitaly@gmail.com>
2017-04-03 07:27:33 -0600

    Removing old timezone utilities. Adding new normalization utility.

--- lib/netsuite/utilities.rb
+++ lib/netsuite/utilities.rb
@@ -214,33 +214,21 @@ module NetSuite
       DataCenter.get(*args)
     end
 
-    # Warning this was developed with a Web Services user whose time zone was set to CST
-    # the time zone setting of the user seems to effect how times work in NS
+    # http://mikebian.co/notes-on-dates-timezones-with-netsuites-suitetalk-api/
+    # assumes UTC0 unix timestamp
+    def normalize_time_to_netsuite_date(unix_timestamp)
+      # convert to date to eliminate hr/min/sec
+      time = Time.at(unix_timestamp).utc.to_date.to_datetime
 
-    # modifying time without rails:
-    # http://stackoverflow.com/questions/238684/subtract-n-hours-from-a-datetime-in-ruby
+      offset = 8
+      time = time.new_offset("-08:00")
 
-    # converting between time and datetime:
-    # http://stackoverflow.com/questions/279769/convert-to-from-datetime-and-time-in-ruby
+      if time.to_time.dst?
+        offset = 7
+        time = time.new_offset("-07:00")
+      end
 
-    # use when sending times to NS
-    def normalize_datetime_to_netsuite(datetime)
-      # normalize the time to UCT0
-      # add 6 hours (21600 seconds) of padding (CST offset)
-      # to force the same time to be displayed in the NS UI
-
-      is_dst = Time.parse(datetime.to_s).dst?
-
-      datetime.new_offset(0) + datetime.offset + Rational(is_dst ? 5 : 6, 24)
-    end
-
-    # use when displaying times from a NS record
-    def normalize_datetime_from_netsuite(datetime)
-      # the code below eliminates the TimeZone offset then shifts the date forward 2 hours (7200 seconds)
-      # this ensures that ActiveRecord is given a UTC0 DateTime with the exact hour that
-      # was displayed in the NS UI (CST time zone), which will result in the correct display on the web side
-
-      datetime.new_offset(0) + datetime.offset + Rational(2, 24)
+      (time + Rational(offset, 24)).iso8601
     end
 
   end
