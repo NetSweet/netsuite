2a6dae8c646a7297ab2d058b65744b73778d0238
Michael Bianco <iloveitaly@gmail.com>
2017-05-03 16:22:28 -0600

    Revert "Adding search to SalesTaxItem"
    This reverts commit dd9f570096f3b36cefbfa557f67bb23a9e3d8e54.

--- lib/netsuite/records/sales_tax_item.rb
+++ lib/netsuite/records/sales_tax_item.rb
@@ -8,7 +8,7 @@ module NetSuite
       include Namespaces::ListAcct
 
       actions :get, :get_list, :get_all, :get_select_value, :add, :delete,
-        :update, :upsert, :search
+        :update, :upsert
 
       fields :item_id, :display_name, :description, :rate, :is_inactive,
         :effective_from, :valid_until, :include_children, :eccode,
@@ -16,23 +16,16 @@ module NetSuite
         :exclude_from_tax_reports, :available, :export, :county, :city, :state,
         :zip
 
-      field :custom_field_list, CustomFieldList
-
       record_refs :tax_type, :tax_agency, :tax_account, :purchase_account,
         :sale_account, :parent, :nexus_country
 
       attr_reader :internal_id
       attr_accessor :external_id
-      attr_accessor :search_joins
 
       def initialize(attributes = {})
         @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
         @external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
         initialize_from_attributes_hash(attributes)
-      end
-
-      def self.search_class_name
-        "Item"
       end
     end
   end
