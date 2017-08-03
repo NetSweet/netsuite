6ced2b85e8864af37a23f7f7e080c0872241b0ef
Carlos Lazo <carlos.jose.lazo@gmail.com>
2017-08-03 14:08:39 -0400

    :art: Alphabetize and order NetSuite::Records::Employee attrs.
    This will make it more readable to add attributes in the future.

--- lib/netsuite/records/employee.rb
+++ lib/netsuite/records/employee.rb
@@ -9,17 +9,83 @@ module NetSuite
 
       # https://system.netsuite.com/help/helpcenter/en_US/srbrowser/Browser2014_1/script/record/employee.html
 
-      actions :get, :get_list, :add, :update, :upsert, :upsert_list, :delete, :search
+      ACTIONS = %w{
+        add
+        delete
+        get
+        get_list
+        search
+        update
+        upsert
+        upsert_list
+      }.map(&:to_sym).freeze
 
-      fields :alt_name, :phone, :first_name, :last_name, :is_inactive, :email, :give_access, :send_email, :is_support_rep,
-             :birth_date, :hire_date, :last_review_date, :next_review_date, :title, :home_phone, :office_phone,
-             :eligible_for_commission, :is_sales_rep, :klass, :middle_name, :account_number, :alien_number, :approval_limit,
-             :bill_pay, :comments, :date_created, :direct_deposit, :entity_id, :password, :password2,
-             :expense_limit, :fax, :is_job_resource, :job_description, :labor_cost, :last_modified_date, :mobile_phone, :pay_frequency,
-             :phonetic_name, :purchase_order_approval_limit, :purchase_order_approver, :purchase_order_limit, :release_date,
-             :resident_status, :salutation, :social_security_number, :visa_exp_date, :visa_type
+      actions *ACTIONS
 
-      record_refs :currency, :department, :location, :subsidiary, :employee_type, :employee_status, :supervisor
+      FIELDS = %w{
+        account_number
+        alien_number
+        alt_name
+        approval_limit
+        bill_pay
+        birth_date
+        comments
+        date_created
+        direct_deposit
+        eligible_for_commission
+        email
+        entity_id
+        expense_limit
+        fax
+        first_name
+        give_access
+        hire_date
+        home_phone
+        is_inactive
+        is_job_resource
+        is_sales_rep
+        is_support_rep
+        job_description
+        klass
+        labor_cost
+        last_modified_date
+        last_name
+        last_review_date
+        middle_name
+        mobile_phone
+        next_review_date
+        office_phone
+        password
+        password2
+        pay_frequency
+        phone
+        phonetic_name
+        purchase_order_approval_limit
+        purchase_order_approver
+        purchase_order_limit
+        release_date
+        resident_status
+        salutation
+        send_email
+        social_security_number
+        title
+        visa_exp_date
+        visa_type
+      }.map(&:to_sym).freeze
+
+      fields *FIELDS
+
+      RECORD_REFS = %w{
+        currency
+        department
+        employee_status
+        employee_type
+        location
+        subsidiary
+        supervisor
+      }.map(&:to_sym).freeze
+
+      record_refs *RECORD_REFS
 
       field :custom_field_list, CustomFieldList
       field :roles_list, RoleList
