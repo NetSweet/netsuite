1da08427d9b39d4781e02ecfbe63f743c84b0839
Carlos Lazo <carlos.jose.lazo@gmail.com>
2017-08-03 14:09:18 -0400

    :art: Clean up NetSuite::Records::Employee spec.
    In the original implementation, fields were added to the attribute lists in NetSuite::Records::Employee but never added to the specs, so there was a disparity. Cleaned up this spec to use the lists directly from the class.

--- spec/netsuite/records/employee_spec.rb
+++ spec/netsuite/records/employee_spec.rb
@@ -3,26 +3,14 @@ require 'spec_helper'
 describe NetSuite::Records::Employee do
   let(:employee) { NetSuite::Records::Employee.new }
 
-  it 'has all the right fields' do
-    [
-      :account_number, :alien_number, :approval_limit, :bill_pay, :birth_date,
-      :comments, :date_created, :direct_deposit, :eligible_for_commission,
-      :email, :entity_id, :expense_limit,
-      :fax, :first_name, :hire_date, :home_phone, :is_inactive, :is_job_resource,
-      :job_description, :labor_cost, :last_modified_date, :last_name, :middle_name,
-      :mobile_phone, :next_review_date, :office_phone, :pay_frequency, :phone,
-      :phonetic_name, :purchase_order_approval_limit, :purchase_order_approver,
-      :purchase_order_limit, :release_date, :resident_status, :salutation,
-      :social_security_number, :title, :visa_exp_date, :visa_type
-    ].each do |field|
+  it 'has all the expected fields' do
+    NetSuite::Records::Employee::FIELDS.each do |field|
       expect(employee).to have_field(field)
     end
   end
 
-  it 'has all the right record refs' do
-    [
-      :location, :employee_status, :employee_type
-    ].each do |record_ref|
+  it 'has all the expected record_refs' do
+    NetSuite::Records::Employee::RECORD_REFS.each do |record_ref|
       expect(employee).to have_record_ref(record_ref)
     end
   end
@@ -32,7 +20,7 @@ describe NetSuite::Records::Employee do
     it 'can be set from a RoleList object'
   end
 
-  describe '.get' do
+  describe '#get' do
     context 'when the response is successful' do
       let(:response) { NetSuite::Response.new(:success => true, :body => { :account_number => 7 }) }
 
@@ -106,7 +94,7 @@ describe NetSuite::Records::Employee do
     end
   end
 
-  describe '.update' do
+  describe '#update' do
     context 'when the response is successful' do
       let(:response) { NetSuite::Response.new(:success => true, :body => { :email => 'leland.palmer@example.com' }) }
 
@@ -139,14 +127,6 @@ describe NetSuite::Records::Employee do
   describe '#record_type' do
     it 'returns a string type for the record to be used in a SOAP request' do
       expect(employee.record_type).to eql('listEmp:Employee')
-    end
-  end
-
-  it 'has the right record_refs' do
-    [
-      :currency, :department, :location, :subsidiary
-    ].each do |record_ref|
-      expect(employee).to have_record_ref(record_ref)
     end
   end
 end
