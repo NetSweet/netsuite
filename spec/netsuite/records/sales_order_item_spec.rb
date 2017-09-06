6e65fd4da8aa1399c580129cca95d64b8146381e
Julien Vanier <julien@particle.io>
2017-09-06 17:02:13 -0400

    Add additional quantities to sales order item

--- spec/netsuite/records/sales_order_item_spec.rb
+++ spec/netsuite/records/sales_order_item_spec.rb
@@ -10,7 +10,9 @@ describe NetSuite::Records::SalesOrderItem do
      :gift_cert_from, :gift_cert_message, :gift_cert_number,
      :gift_cert_recipient_email, :gift_cert_recipient_name,
      :gross_amt, :is_taxable, :line, :order_line, :po_currency,
-     :quantity, :rate, :rev_rec_end_date, :rev_rec_start_date,
+     :quantity, :quantity_back_ordered, :quantity_billed,
+     :quantity_committed, :quantity_fulfilled,
+     :rate, :rev_rec_end_date, :rev_rec_start_date,
      :rev_rec_term_in_months, :serial_numbers, :tax1_amt, :tax_rate1,
      :tax_rate2, :vsoe_allocation, :vsoe_amount, :vsoe_deferral,
      :vsoe_delivered, :vsoe_permit_discount, :vsoe_price
