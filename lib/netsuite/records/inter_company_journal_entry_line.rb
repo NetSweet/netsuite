48e0abe9bd2538bf19cf6930c15d4fc73536af3b
Taylor Carr <taylor@catamountsolutions.com>
2017-11-27 17:50:28 -0500

    Add line level subsidiary field

--- lib/netsuite/records/inter_company_journal_entry_line.rb
+++ lib/netsuite/records/inter_company_journal_entry_line.rb
@@ -8,7 +8,7 @@ module NetSuite
 
       fields :credit, :debit, :eliminate, :end_date, :gross_amt, :memo, :residual, :start_date, :tax1_amt, :tax_rate1
       field :custom_field_list, CustomFieldList
-      record_refs :account, :department, :entity, :klass, :location, :schedule, :schedule_num, :tax1_acct, :tax_code
+      record_refs :account, :department, :entity, :klass, :line_subsidiary, :location, :schedule, :schedule_num, :tax1_acct, :tax_code
 
       def initialize(attributes = {})
         initialize_from_attributes_hash(attributes)
