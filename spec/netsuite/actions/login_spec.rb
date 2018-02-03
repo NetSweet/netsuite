7334dab86a56adda64eef64b53536761ebb08e3b
Michael Bianco <iloveitaly@gmail.com>
2018-02-03 08:52:01 -0700

    Adding test for login call when tba is enabled

--- spec/netsuite/actions/login_spec.rb
+++ spec/netsuite/actions/login_spec.rb
@@ -41,4 +41,27 @@ describe NetSuite::Actions::Login do
       role: 234
     }) }.to raise_error(Savon::SOAPFault)
   end
+
+  it 'handles a login call when token based auth is in place' do
+    NetSuite.configure do
+      consumer_key '123'
+      consumer_secret '123'
+      token_id '123'
+      token_secret '123'
+
+      api_version '2017_2'
+    end
+
+    message = {"platformMsgs:passport"=>{"platformCore:email"=>"email", "platformCore:password"=>"password", "platformCore:account"=>"1234", "platformCore:role"=>234}}
+    savon.expects(:login).with(:message => message).returns(File.read('spec/support/fixtures/login/success.xml'))
+
+    result = NetSuite::Actions::Login.call({
+      email: 'email',
+      password: 'password',
+      role: 234
+    })
+
+    expect(result.success?).to eq(true)
+    expect(result.body[:user_id]).to_not be_nil
+  end
 end
