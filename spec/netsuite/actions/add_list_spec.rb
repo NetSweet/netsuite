aa17e2dc22a35ad60de2a8a7907d2cc783ab9a92
Mika Marttila <mika@oranssi.org>
2018-08-16 16:20:21 +0300

    Test for duplicate entity

--- spec/netsuite/actions/add_list_spec.rb
+++ spec/netsuite/actions/add_list_spec.rb
@@ -74,7 +74,7 @@ describe NetSuite::Actions::AddList do
     end
   end
 
-  context 'with errors' do
+  context 'with validation errors' do
     let(:customers) do
       [
         NetSuite::Records::Customer.new(external_id: 'ext1-bad', entity_id: 'Shutter Fly', company_name: 'Shutter Fly, Inc.'),
@@ -103,9 +103,45 @@ describe NetSuite::Actions::AddList do
 
     it 'constructs error objects' do
       response = NetSuite::Actions::AddList.call(customers)
+      expect(response.errors.keys).to match_array(['ext1-bad', 'ext2-bad'])
+      expect(response.errors['ext1-bad'].first.code).to eq('USER_ERROR')
+      expect(response.errors['ext1-bad'].first.message).to eq('Please enter value(s) for: Item')
+      expect(response.errors['ext1-bad'].first.type).to eq('ERROR')
+    end
+  end
+
+  context 'with existing record errors' do
+    let(:customers) do
+      [
+        NetSuite::Records::Customer.new(external_id: 'ext1', entity_id: 'Shutter Fly', company_name: 'Shutter Fly, Inc.'),
+        NetSuite::Records::Customer.new(external_id: 'ext2', entity_id: 'Target', company_name: 'Target')
+      ]
+    end
+
+    before do
+      savon.expects(:add_list).with(:message =>
+        {
+          'record' => [{
+            'listRel:entityId'    => 'Shutter Fly',
+            'listRel:companyName' => 'Shutter Fly, Inc.',
+            '@xsi:type' => 'listRel:Customer',
+            '@externalId' => 'ext1'
+          },
+          {
+            'listRel:entityId'    => 'Target',
+            'listRel:companyName' => 'Target',
+            '@xsi:type' => 'listRel:Customer',
+            '@externalId' => 'ext2'
+          }
+          ]
+        }).returns(File.read('spec/support/fixtures/add_list/add_list_with_existing_records.xml'))
+    end
+
+    it 'constructs error objects' do
+      response = NetSuite::Actions::AddList.call(customers)
       expect(response.errors.keys).to match_array(['ext1', 'ext2'])
-      expect(response.errors['ext1'].first.code).to eq('USER_ERROR')
-      expect(response.errors['ext1'].first.message).to eq('Please enter value(s) for: Item')
+      expect(response.errors['ext1'].first.code).to eq('DUP_ENTITY')
+      expect(response.errors['ext1'].first.message).to eq('This entity already exists.')
       expect(response.errors['ext1'].first.type).to eq('ERROR')
     end
   end
