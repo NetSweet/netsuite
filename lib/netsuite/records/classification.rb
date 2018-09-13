05b8a16a60a26a35e4124bf9b315b9fef046c2fa
Michael Bianco <iloveitaly@gmail.com>
2018-09-13 08:03:59 -0600

    Adding missing add action on classification. Fixes #409

--- lib/netsuite/records/classification.rb
+++ lib/netsuite/records/classification.rb
@@ -7,7 +7,7 @@ module NetSuite
       include Support::Actions
       include Namespaces::ListAcct
 
-      actions :get, :get_list, :delete, :upsert, :search
+      actions :add, :get, :get_list, :delete, :upsert, :search
 
       fields :name, :include_children, :is_inactive, :class_translation_list, :custom_field_list, :parent
 
