NetSweet/netsuite

commit 399fb4dd44acf87db23bc6ae5d661cd5ae40d526

Do not overwrite 'external_id' attr in update, if it is provided in options

lib/netsuite/actions/update.rb
@@ -71,8 +71,12 @@ module NetSuite
 
       module Support
         def update(options = {}, credentials={})
-          options.merge!(:internal_id => internal_id) if respond_to?(:internal_id) && internal_id
-          options.merge!(:external_id => external_id) if respond_to?(:external_id) && external_id
+          options[:internal_id] = internal_id if respond_to?(:internal_id) && internal_id
+
+          if !options.include?(:external_id) || (respond_to?(:external_id) && external_id)
+            options[:external_id] = external_id
+          end
+
           response = NetSuite::Actions::Update.call([self.class, options], credentials)
           @errors = response.errors
           response.success?

commit 2b979be2f5608ea79d11b8bf62567f632fd3640f

Fixing external ID being passed as nil on update
Regression caused by https://github.com/NetSweet/netsuite/pull/416

lib/netsuite/actions/update.rb
@@ -73,7 +73,7 @@ module NetSuite
         def update(options = {}, credentials={})
           options[:internal_id] = internal_id if respond_to?(:internal_id) && internal_id
 
-          if !options.include?(:external_id) || (respond_to?(:external_id) && external_id)
+          if !options.include?(:external_id) && (respond_to?(:external_id) && external_id)
             options[:external_id] = external_id
           end
 