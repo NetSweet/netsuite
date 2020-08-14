cc6c3187968cb86d1861b2e76e45b340b6048e58
Neil Kulbiski <neil.kulbiski@backlotcars.com>
2020-08-14 13:41:00 -0500

    add subsidiary to partner record

--- lib/netsuite/records/partner.rb
+++ lib/netsuite/records/partner.rb
@@ -16,7 +16,7 @@ module NetSuite
              :partner_code, :is_person, :company_name, :eligible_for_commission, :entity_id, :last_modified_date,
              :date_created, :title, :mobile_phone, :comments, :middle_name, :send_email, :password, :password2
 
-      record_refs :klass, :access_role, :department
+      record_refs :klass, :access_role, :department, :subsidiary
 
       attr_reader   :internal_id
       attr_accessor :external_id
