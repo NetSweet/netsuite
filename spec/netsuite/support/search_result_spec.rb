197d4e63e85d6a9fcd0ee0d45a1ee8a8b0ece05c
Michael Bianco <iloveitaly@gmail.com>
2022-03-15 21:01:52 -0600

    Adding test for single result return

--- spec/netsuite/support/search_result_spec.rb
+++ spec/netsuite/support/search_result_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe NetSuite::Support::SearchResult do
+  before(:all) { savon.mock! }
+  after(:all) { savon.unmock! }
+
   describe '#results' do
     context 'empty page' do
       it 'returns empty array' do
@@ -19,6 +22,15 @@ describe NetSuite::Support::SearchResult do
         results = described_class.new(response, NetSuite::Actions::Search, {}).results
         expect(results).to eq []
       end
+    end
+
+    it 'handles a recordList with a single element' do
+      response = File.read('spec/support/fixtures/search/single_search_result.xml')
+      savon.expects(:search).with(message: {}).returns(response)
+
+      results = NetSuite::Records::Account.search(basic: [])
+
+      expect(results.results.count).to eq 1
     end
   end
 end
