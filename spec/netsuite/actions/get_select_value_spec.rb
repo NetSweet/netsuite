cdbfac508016bf0e70354392cffb3c131c4356e4
Wojtek Kruszewski <wojtek@kruszewski.com>
2022-05-23 11:10:49 -0400

    Reproduce an exception with empty result

--- spec/netsuite/actions/get_select_value_spec.rb
+++ spec/netsuite/actions/get_select_value_spec.rb
@@ -9,6 +9,8 @@ describe NetSuite::Actions::GetSelectValue do
       NetSuite::Records::BaseRefList.get_select_value(recordType: 'itemFulfillment', field: 'shipMethod')
     end
 
+    let(:response) { File.read('spec/support/fixtures/get_select_value/item_fulfillment_ship_method.xml') }
+
     before do
       savon.expects(:get_select_value).with(:message => {
         pageIndex: 1,
@@ -16,7 +18,7 @@ describe NetSuite::Actions::GetSelectValue do
           'platformCore:recordType' => 'itemFulfillment',
           'platformCore:field' => 'shipMethod'
         }
-      }).returns(File.read('spec/support/fixtures/get_select_value/item_fulfillment_ship_method.xml'))
+      }).returns(response)
     end
 
     it 'makes a valid request to the NetSuite API' do
@@ -27,6 +29,23 @@ describe NetSuite::Actions::GetSelectValue do
       expect(subject).to be_kind_of(NetSuite::Records::BaseRefList)
       expect(subject.base_refs[0].internal_id).to eq('94')
       expect(subject.base_refs[0].name).to eq('Ground (Custom)')
+    end
+
+    context 'with empty result' do
+      let(:response) { File.read('spec/support/fixtures/get_select_value/empty_result.xml') }
+
+      it 'returns an empty list' do
+        # NoMethodError:
+        #   private method `select' called for nil:NilClass
+        # # ./lib/netsuite/support/attributes.rb:14:in `initialize_from_attributes_hash'
+        # # ./lib/netsuite/support/sublist.rb:34:in `initialize'
+        # # ./lib/netsuite/actions/get_select_value.rb:52:in `new'
+        # # ./lib/netsuite/actions/get_select_value.rb:52:in `get_select_value'
+        # # ./spec/netsuite/actions/get_select_value_spec.rb:9:in `block (3 levels) in <top (required)>'
+        # # ./spec/netsuite/actions/get_select_value_spec.rb:38:in `block (4 levels) in <top (required)>'
+        expect(subject).to be_kind_of(NetSuite::Records::BaseRefList)
+        expect(subject.base_refs).to be_empty
+      end
     end
   end
 end
