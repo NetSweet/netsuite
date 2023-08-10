db6b61c4df83dd24df35e89113094b1f1faadb3d
Madeline Wu <madeline@settle.co>
2023-08-09 17:41:30 -0700

    Add MarkupItem and ServicePurchaseItem

---
+++ lib/netsuite/records/markup_item.rb
@@ -0,0 +1,43 @@
+module NetSuite
+  module Records
+    class MarkupItem
+      include Support::Fields
+      include Support::RecordRefs
+      include Support::Records
+      include Support::Actions
+      include Namespaces::ListAcct
+
+      actions :get, :get_deleted, :get_list, :add, :delete, :search, :upsert, :update, :update_list
+
+      fields :cost,
+             :created_date,
+             :display_name,
+             :is_inactive,
+             :item_id,
+             :last_modified_date,
+             :purchase_description
+
+      record_refs :klass,
+                  :department,
+                  :expense_account,
+                  :location,
+                  :tax_schedule
+
+      field :custom_field_list, CustomFieldList
+      field :item_vendor_list, ItemVendorList
+
+      attr_reader   :internal_id
+      attr_accessor :external_id
+
+      def initialize(attributes = {})
+        @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
+        @external_id = attributes.delete(:external_id) || attributes.delete(:@external_id)
+        initialize_from_attributes_hash(attributes)
+      end
+
+      def self.search_class_name
+        "Item"
+      end
+    end
+  end
+end
