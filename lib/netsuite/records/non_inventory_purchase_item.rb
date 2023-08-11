a894a1945784ff6708d78fd42d1285f79095ee96
Grey Campbell <grey@settle.co>
2023-08-11 12:46:59 -0500

    Added update action to non-inventory purchase item

--- lib/netsuite/records/non_inventory_purchase_item.rb
+++ lib/netsuite/records/non_inventory_purchase_item.rb
@@ -7,7 +7,7 @@ module NetSuite
       include Support::Actions
       include Namespaces::ListAcct
 
-      actions :get, :get_deleted, :get_list, :add, :delete, :search, :update_list, :upsert
+      actions :get, :get_deleted, :get_list, :add, :delete, :search, :update_list, :upsert, :update
 
       fields :available_to_partners, :cost, :cost_estimate, :cost_estimate_type, :cost_estimate_units, :country_of_manufacture,
         :created_date, :display_name, :dont_show_price, :enforce_min_qty_internally, :exclude_from_sitemap,
