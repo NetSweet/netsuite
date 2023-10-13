f4990b25c4e8cf35cca7c0668c4e688ee555daf4
Howell Manongsong <24601519+hmanongsong@users.noreply.github.com>
2023-10-13 15:32:56 -0400

    Add support to Inventory Status field

--- lib/netsuite/records/inventory_assignment.rb
+++ lib/netsuite/records/inventory_assignment.rb
@@ -6,10 +6,10 @@ module NetSuite
       include Support::Fields
       include Namespaces::PlatformCommon
 
-      fields :date_time, :quantity, :quantity_available, :expiration_date, 
+      fields :date_time, :quantity, :quantity_available, :expiration_date,
         :receipt_inventory_number
 
-      record_refs :bin_number, :issue_inventory_number, :to_bin_number
+      record_refs :bin_number, :issue_inventory_number, :to_bin_number, :inventory_status
 
       attr_reader   :internal_id
       attr_accessor :external_id
