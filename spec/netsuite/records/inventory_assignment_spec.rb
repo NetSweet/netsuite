3105d42cd042ef9a8fef089b021964a3301c0efc
Howell Manongsong <24601519+hmanongsong@users.noreply.github.com>
2023-10-14 10:57:27 -0400

    Add support to Inventory Status field (#596)
    * Add support to Inventory Status field
    
    * Add inventory status ref to spec

--- spec/netsuite/records/inventory_assignment_spec.rb
+++ spec/netsuite/records/inventory_assignment_spec.rb
@@ -13,7 +13,7 @@ describe NetSuite::Records::InventoryAssignment do
 
   it 'has all the right record refs' do
     [
-      :bin_number, :issue_inventory_number, :to_bin_number
+      :bin_number, :issue_inventory_number, :to_bin_number, :inventory_status
     ].each do |record_ref|
       expect(item).to have_record_ref(record_ref)
     end
